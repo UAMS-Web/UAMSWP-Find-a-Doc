768b8120edbb9610f0dfff98de950da93295cbd8
todd-uams <todd@uams.edu>
2019-08-14 15:28:40 -0500

    Make NPI required

--- includes/class.fad-physician-acf.php
+++ includes/class.fad-physician-acf.php
@@ -738,7 +738,7 @@ if( function_exists('acf_add_local_field_group') ):
         'name' => 'physician_npi',
         'type' => 'text',
         'instructions' => '',
-        'required' => 0,
+        'required' => 1,
         'conditional_logic' => 0,
         'wrapper' => array(
           'width' => '50',
