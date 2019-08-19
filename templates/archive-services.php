80232211e82ad7a10757a05e334ffd1876177ad4
todd-uams <todd@uams.edu>
2019-08-19 17:23:58 -0500

    Service loop

--- templates/archive-services.php
+++ templates/archive-services.php
@@ -2,95 +2,35 @@
 
   add_filter( 'facetwp_template_use_archive', '__return_true' );
 
-  // get_template_part( 'header', 'image' ); ?>
+ ?>
 
-  <div class="container uams-body">
-
+<main class="container-fluid p-8 p-sm-10 area-of-expertise-list bg-auto">
     <div class="row">
-
-      <div class="col-md-12 uams-content" role='main'>
-
-        <?php // uams_site_title(); ?>
-
-      <?php // Hard coded breadcrumbs ?>
-      <nav class="uams-breadcrumbs" role="navigation" aria-label="breadcrumbs">
-        <ul>
-          <li><a href="http://www.uams.edu" title="University of Arkansas for Medical Sciences">Home</a></li>
-          <li><a href="/" title="<?php echo str_replace('   ', ' ', get_bloginfo('title')); ?>"><?php echo str_replace('   ', ' ', get_bloginfo('title')); ?></a></li>
-          <li class="current" title="Services"><span>Services</span></li>
-        </ul>
-      </nav>
-
-        <div id='main_content' class="uams-body-copy" tabindex="-1">
-
-          <h1>Clinical Services</h1><hr>
-          <?php
-            // Start the Loop.
-            $service_posts = new WP_Query( array(
-              'posts_per_page' => -1,
-              'post_type' => 'services',
-              'post_parent' => 0, // required
-              'orderby' => 'title',
-              'order' => 'ASC',
-              // 'paged' => $paged,
-              // 'post__not_in' => $do_not_duplicate 
-            ) );
-
-            if ( $service_posts->have_posts() ) : while ( $service_posts->have_posts() ) : $service_posts->the_post();
-                if ( get_post_meta( get_the_ID(), 'show_image_single', true ) && is_single() && get_post_thumbnail_id() ) { ?>
-                    <p class="featured-image">
-                    <a href="<?php echo get_the_post_thumbnail_url( get_the_ID(),'full' ); ?>" title="<?php echo get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true ); ?>" data-title="<?php echo get_post_field( 'post_title', get_post_thumbnail_id() ); ?>" data-caption="<?php echo get_post_field( 'post_excerpt', get_post_thumbnail_id() ); ?>">
-                        <span class="screen-reader-text"><?php esc_attr_e( 'View Larger Image', 'UAMS' ); ?></span>
-                    <?php the_post_thumbnail( 'post-image' ); ?>
-                    <?php if ( get_post_field('post_excerpt', get_post_thumbnail_id() )) { ?>
-                        <br/><span class="wp-caption-text"><?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?></span>
-                        <?php } ?>
-                    </a>
-                    </p>
-                <?php
-                }
-                echo '<h3><a href="'. get_post_permalink() .'">'. get_the_title() .'</a></h3>';
-                the_post_thumbnail( array(130, 130), array( 'class' => 'attachment-post-thumbnail blogroll-img' ) );
-                the_excerpt();
-                echo "<hr>";
-            endwhile;
-            endif;
-          ?>
-        <!-- <div class="row">
-              <div class="col-md-4">
-                <?php //echo do_shortcode( '[wpdreams_ajaxsearchpro id=1]' ); ?>
-                <?php //echo do_shortcode( '[accordion]
-                        //       [section title="Advanced Filter" active="true"]     
-                        //       <div class="fwp-filter">[facetwp facet="specialties"]</div>
-                        //     <button onclick="FWP.reset()">Reset</button>
-                        //     [/section]
-                        //   [/accordion]' ); ?>
-              </div>
-          <div class="col-md-8 people">
-            <?php //echo facetwp_display( 'facet', 'alpha' ); ?>
-            <div class="row">
-              <div class="col-md-8 text-center fwp-counts">
-                <?php //echo facetwp_display( 'counts' ); ?> Locations
-              </div>
-              <div class="col-md-4 text-right">
-                <?php //echo facetwp_display( 'sort' ); ?>
-              </div>
+        <div class="col-12">
+            <h1 class="page-title">Services / Areas of Expertise</h1>
+            <div class="module-body">
+                <p class="lead"><strong><a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-visual-presentation.html" target="_blank">WCAG 2.0 SC 1.4.8</a> Visual Presentation:</strong> For the visual presentation of blocks of text, a mechanism is available to achieve the following: (Level AAA)</p>
+                <ul>
+                    <li>Foreground and background colors can be selected by the user.</li>
+                    <li><strong>Width is no more than 80 characters or glyphs</strong> (40 if <abbr title="CJK is a collective term for the Chinese, Japanese, and Korean languages">CJK</abbr>).</li>
+                    <li>Text is not justified (aligned to both the left and the right margins).</li>
+                    <li>Line spacing (leading) is at least space-and-a-half within paragraphs, and paragraph spacing is at least 1.5 times larger than the line spacing.</li>
+                    <li>Text can be resized without assistive technology up to 200 percent in a way that does not require the user to scroll horizontally to read a line of text on a full-screen window.</li>
+                </ul>
             </div>
-            <hr>
-            <?php //echo facetwp_display( 'template', 'locations' ); ?> -->
-            <?php //get_template_part( 'templates/physician-loop' ); ?>
-            <?php //echo facetwp_display( 'pager' ); ?>
-            <?php //echo do_shortcode('[facetwp load_more="true" label="Load more"]'); ?>
-          <!--</div> .col -->
-        <!--</div> .row -->
+            <h2 class="sr-only">List of Areas of Expertise</h2>
+            <div class="card-list-container">
+                <div class="card-list">
+                    <?php echo facetwp_display( 'template', 'services' ); ?>
+                    <?php //get_template_part( 'templates/physician-loop' ); ?>
+                    <?php //echo facetwp_display( 'pager' ); ?>
+                    <?php //echo do_shortcode('[facetwp load_more="true" label="Load more"]'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
-        </div><!-- main_content -->
 
-      </div><!-- uams-content -->
-    <div id="sidebar"></div>
-
-  </div>
-
-</div>
 
 <?php get_footer(); ?>
---
+++ templates/fwp/service-loop.php
@@ -0,0 +1,20 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div class="card">
+		<?php if ( has_post_thumbnail() ) { ?>
+			<p>
+			<?php the_post_thumbnail('aspect-16-9-small', ['class' => 'img-responsive']); ?>
+			</p>
+		<?php } ?>
+		<?php $excerpt = get_the_excerpt(); ?>
+		<div class="card-body">
+            <h3 class="card-title">
+				<span class="name"><a href="<?php echo get_permalink(); ?>" target="_self"><?php the_title(); ?></a></span>
+            </h3>
+			<p class="card-text"><?php echo ( $excerpt ? wp_trim_words( $excerpt, 30, ' &hellip;' ) : wp_trim_words( wp_strip_all_tags( get_the_content(), 30, ' &hellip;' ) ) ); ?></p>
+				<a href="<?php echo get_permalink(); ?>" class="btn btn-primary stretched-link" aria-label="Go to Area of Expertise page for <?php the_title(); ?>">View Area of Expertise</a>
+		</div><!-- .card-body -->
+	</div><!-- .card --> 
+<?php endwhile; else : ?>
+<?php  ?>
+	<p><?php _e( 'Sorry, no locations matched your criteria.' ); ?></p>
+<?php endif; ?>
