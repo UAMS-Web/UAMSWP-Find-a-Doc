1a78dec83a37bd902f315b4351bd7eac9ce45cf0
todd-uams <todd@uams.edu>
2019-09-25 08:27:35 -0500

    Create class.fad-settings-pages.php

---
+++ includes/class.fad-settings-pages.php
@@ -0,0 +1,86 @@
+<?php 
+
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+		'page_title' 	=> 'Find-a-Doc Settings',
+		'menu_title'	=> 'Find-a-Doc Settings',
+		'menu_slug' 	=> 'fad-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+    ));
+    
+    acf_add_options_sub_page(array(
+		'page_title' 	=> 'Services Options',
+        'menu_title'	=> 'Clinical Services',
+        'menu_slug' 	=> 'uamswp-fad-services',
+        'parent_slug'	=> 'fad-settings',
+        'redirect'		=> false
+    ));
+    
+    acf_add_options_sub_page(array(
+		'page_title' 	=> 'Taxonomy Options',
+        'menu_title'	=> 'Taxonomy Options',
+        'menu_slug' 	=> 'uamswp-fad-tax',
+        'parent_slug'	=> 'fad-settings',
+        'redirect'		=> false
+    ));
+
+}
+
+// Add metaboxes for Settings page
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_uams_theme_settings',
+        'title' => 'Theme Settings',
+        'fields' => array(
+            array(
+                'key' => 'field_fad_google_key',
+                'label' => 'Google Maps API Key',
+                'name' => 'fad_google_key',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '50',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'fad-settings',
+                ),
+            ),
+        ),
+        'menu_order' => 5,
+        'position' => 'normal',
+        'style' => 'seamless',
+        'label_placement' => 'left',
+        'instruction_placement' => 'label',
+        'active' => true,
+    ));
+
+endif;
+
+
+
+function my_acf_google_key() {
+    $key = get_field('fad_google_key', 'option');
+    if ($key) {
+        acf_update_setting('google_api_key', $key); 
+    }
+    // echo "<script> console.log('PHP: ".$key ."');</script>";
+}
+add_action('acf/init', 'my_acf_google_key');
