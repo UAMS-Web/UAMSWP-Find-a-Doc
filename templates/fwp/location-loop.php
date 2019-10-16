29e5da563441bd0636d5426b7efcf62cbd6c2f9b
todd-uams <todd@uams.edu>
2019-10-16 12:20:41 -0500

    Remove $arg from get_field

--- templates/fwp/location-loop.php
+++ templates/fwp/location-loop.php
@@ -10,9 +10,9 @@
 				<span class="name"><a href="<?php echo get_permalink(); ?>" target="_self"><?php the_title(); ?></a></span>
             </h3>
 			<?php $map = get_field('location_map'); ?>
-			<p class="card-text"><?php echo get_field('location_address_1', $args, get_the_ID() ); ?><br/>
-				<?php echo ( get_field('location_address_2', $args ) ? get_field('location_address_2', $args) . '<br/>' : ''); ?>
-				<?php echo get_field('location_city', $args); ?>, <?php echo get_field('location_state', $args); ?> <?php echo get_field('location_zip', $args, get_the_ID()); ?><p/>
+			<p class="card-text"><?php echo get_field('location_address_1', get_the_ID() ); ?><br/>
+				<?php echo ( get_field('location_address_2' ) ? get_field('location_address_2') . '<br/>' : ''); ?>
+				<?php echo get_field('location_city'); ?>, <?php echo get_field('location_state'); ?> <?php echo get_field('location_zip', get_the_ID()); ?><p/>
 				<a href="<?php echo get_permalink(); ?>" class="btn btn-primary" aria-label="Go to location page for <?php the_title(); ?>">View Location</a>
 				<?php if ($map) { ?>
 				<a class="btn btn-outline-primary" href="https://www.google.com/maps/dir/Current+Location/<?php echo $map['lat'] ?>,<?php echo $map['lng'] ?>" target="_blank">Get Directions</a>
