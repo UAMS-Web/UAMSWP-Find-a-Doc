10cbd273e9489463ff658d76db30f5e24b3da2ef
todd-uams <todd@uams.edu>
2019-10-17 15:24:03 -0500

    Minor cleanup

--- templates/fwp/location-loop.php
+++ templates/fwp/location-loop.php
@@ -1,10 +1,9 @@
 <?php 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-$id =get_the_ID();
+$id = get_the_ID();
 include( UAMS_FAD_PATH . '/templates/loops/location-card.php' );
 
 endwhile; else : ?>
-<?php  ?>
 	<p><?php _e( 'Sorry, no locations matched your criteria.' ); ?></p>
 <?php endif; ?>
