78b8be20c8410c1118154f58dc30152177bcb1cf
joshdaugherty <joshdaugherty@gmail.com>
2019-10-24 14:59:06 -0500

    Remove appointment CTA bar from locations page

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -389,14 +389,6 @@ while ( have_posts() ) : the_post(); ?>
 			</div>
 		</div>
 	</section>
-	<section class="container-fluid p-8 p-sm-10 cta-bar cta-bar-1 bg-auto">
-		<div class="row">
-			<div class="col-xs-12">
-				<h2>Make an Appointment</h2>
-				<p>Request an appointment directly with <a href="javascript:void(0)">your clinic</a>, <a href="javascript:void(0)">your doctor</a>, <span class="no-break">or call <a href="javascript:void(0)">501-555-5555</a>.</span></p>
-			</div>
-		</div>
-	</section>
 </main>
 
 <?php endwhile; // end of the loop. ?>
