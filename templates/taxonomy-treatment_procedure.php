296270c69b4cfa083078f4e3547cac8fee303b54
joshdaugherty <joshdaugherty@gmail.com>
2019-10-25 15:09:15 -0500

    Wrap the physician card loop in the right structural div

--- templates/taxonomy-treatment_procedure.php
+++ templates/taxonomy-treatment_procedure.php
@@ -54,7 +54,9 @@
 				<h2 class="module-title">Doctors Providing Treatments or Services for <?php echo single_cat_title( '', false ); ?></h2>
 				<p class="note">Note that every treatment/service listed above may not be provided by each doctor listed below. Review each doctor for availability.</p>	
 				<div class="card-list-container">
-					<?php echo facetwp_display( 'template', 'treatment_physicians' ); ?>
+					<div class="card-list card-list-doctors facetwp-template">
+						<?php echo facetwp_display( 'template', 'treatment_physicians' ); ?>
+					</div>
 				</div>
 				<div class="row list-pagination">
 					<div class="col">
