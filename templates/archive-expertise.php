bf8cafdb4cc18ab89c1f76fe56eb870700c50dd5
todd-uams <todd@uams.edu>
2019-10-30 08:59:24 -0500

    Fix pager & change facet to expertise

--- templates/archive-expertise.php
+++ templates/archive-expertise.php
@@ -10,21 +10,22 @@
 <main class="container-fluid p-8 p-sm-10 area-of-expertise-list bg-auto">
     <div class="row">
         <div class="col-12">
-            <h1 class="page-title"><?php echo ($service_title ? $service_title : 'Clinical Services' ); ?></h1>
+            <h1 class="page-title"><?php echo ($service_title ? $service_title : 'Areas of Expertise' ); ?></h1>
             <?php echo ($service_text ? '<div class="module-body">' . $service_text . '</div>' : '' ); ?>
             <h2 class="sr-only">List of Areas of Expertise</h2>
             <div class="card-list-container">
                 <div class="card-list">
-                    <?php echo facetwp_display( 'template', 'services' ); ?>
+                    <?php echo facetwp_display( 'template', 'expertise' ); ?>
                     <?php //get_template_part( 'templates/physician-loop' ); ?>
-                    <?php //echo facetwp_display( 'pager' ); ?>
-                    <?php //echo do_shortcode('[facetwp load_more="true" label="Load more"]'); ?>
+                </div>
+            </div>
+            <div class="row list-pagination">
+                <div class="col">
+					<?php echo facetwp_display( 'pager' ); ?>
                 </div>
             </div>
         </div>
     </div>
 </main>
-
-
 
 <?php get_footer(); ?>
