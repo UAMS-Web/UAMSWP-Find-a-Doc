9f17c14fff9d1e8ee2e49437dafd0ce3d3b3a977
joshdaugherty <joshdaugherty@gmail.com>
2019-11-19 16:05:34 -0600

    Relocate treatment embed to after main content body

--- templates/taxonomy-treatment_procedure.php
+++ templates/taxonomy-treatment_procedure.php
@@ -49,6 +49,11 @@
 					endif;
 				?>
 				<?php echo ( get_field('treatment_procedure_content', $term) ? ''. get_field('treatment_procedure_content', $term) . '' : '' ); ?>
+				<?php if( get_field('treatment_procedure_youtube_link', $term) ) { ?>
+					<div class="embed-responsive embed-responsive-16by9">
+					<?php echo wp_oembed_get( get_field('treatment_procedure_youtube_link', $term) ); ?>
+					</div>
+				<?php } ?>
 			</div>
 		</section>
 		<?php
@@ -63,19 +68,7 @@
 			</div>
 		</section>
 		<?php endif; ?>
-		<?php
-		if(get_field('treatment_procedure_youtube_link')) { ?>
-			<section class="container-fluid p-8 p-sm-10 bg-auto">
-				<div class="row">
-					<div class="col-12">
-						<div class="embed-responsive embed-responsive-16by9">
-							<?php echo wp_oembed_get( get_field( 'treatment_procedure_youtube_link' ) ); ?>
-						</div>
-					</div>
-				</div>
-			</section>
 		<?php 
-		} 
 			$conditions = get_field('treatment_procedure_conditions', $term);
 			
 			if (!empty($conditions) && 0 < count($conditions)) {
