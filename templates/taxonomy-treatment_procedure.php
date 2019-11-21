c5a646ebcb9672cba78aa3cf46cb74e66c6e21a1
joshdaugherty <joshdaugherty@gmail.com>
2019-11-21 10:16:48 -0600

    Code refactoring on treatment's clinical trials section

--- templates/taxonomy-treatment_procedure.php
+++ templates/taxonomy-treatment_procedure.php
@@ -59,16 +59,16 @@
 		<?php
 		$clinical_trials = get_field('treatment_procedure_clinical_trials', $term);
 		if (!empty($clinical_trials)): ?>
-		<section class="uams-module cta-bar cta-bar-1 bg-auto">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-xs-12">
-						<h2>Clinical Trials</h2>
-						<p><a href="https://uams.trialstoday.org/" aria-label="Search UAMS Clinical Trials">Search our clinical trials</a> for those related to <?php echo single_cat_title( '', false ); ?>.</p>
+			<section class="uams-module cta-bar cta-bar-1 bg-auto">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12">
+							<h2>Clinical Trials</h2>
+							<p><a href="https://uams.trialstoday.org/" aria-label="Search UAMS Clinical Trials">Search our clinical trials</a> for those related to <?php echo single_cat_title( '', false ); ?>.</p>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
 		<?php endif; ?>
 		<?php 
 			$conditions = get_field('treatment_procedure_conditions', $term);
