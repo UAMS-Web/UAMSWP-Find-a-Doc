9a7fd61af2dcc6cefe9c0a409b3f8e8b38339d22
todd-uams <todd@uams.edu>
2020-02-20 13:47:55 -0600

    Remove recognition taxonomy

--- includes/class.fad-custom-post.php
+++ includes/class.fad-custom-post.php
@@ -232,7 +232,7 @@ add_action( 'init', 'create_departments_taxonomy', 0 );
 add_action( 'init', 'create_degrees_taxonomy', 0 );
 add_action( 'init', 'create_patient_type_taxonomy', 0 );
 add_action( 'init', 'create_portal_taxonomy', 0 );
-add_action( 'init', 'create_recognition_taxonomy', 0 );
+// add_action( 'init', 'create_recognition_taxonomy', 0 );
 add_action( 'init', 'create_service_line_taxonomy', 0 );
 // add_action( 'init', 'create_medical_specialties_taxonomy', 0 ); // Disabled
 // add_action( 'init', 'create_medical_terms_taxonomy', 0 ); // Disabled
@@ -1224,22 +1224,7 @@ function create_portal_taxonomy() {
 
 }
 
-<<<<<<< Updated upstream
-//hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_academic_title_taxonomy', 0 );
-
-//create a custom taxonomy name it topics for your posts
 function create_academic_title_taxonomy() {
-
-// Add new taxonomy, make it hierarchical like categories
-// first do the translations part for GUI
-
-=======
-<<<<<<< Updated upstream
-=======
-function create_academic_title_taxonomy() {
-
->>>>>>> Stashed changes
   	$labels = array(
 		'name'                           => 'Academic Titles',
 		'singular_name'                  => 'Academic Title',
@@ -1293,68 +1278,56 @@ function create_academic_title_taxonomy() {
 
 }
 
-<<<<<<< Updated upstream
-//hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_recognition_taxonomy', 0 );
+// function create_recognition_taxonomy() {
 
-=======
->>>>>>> Stashed changes
-function create_recognition_taxonomy() {
+// 	$labels = array(
+// 		'name'                       => 'Recognition Lists',
+// 		'singular_name'              => 'Recognition List',
+// 		'menu_name'                  => 'Recognition Lists',
+// 		'all_items'                  => 'All Recognitions',
+// 		'parent_item'                => 'Parent Recognition',
+// 		'parent_item_colon'          => 'Parent Recognition:',
+// 		'new_item_name'              => 'New Recognition',
+// 		'add_new_item'               => 'Add New Recognition',
+// 		'edit_item'                  => 'Edit Recognition',
+// 		'update_item'                => 'Update Recognition',
+// 		'view_item'                  => 'View Recognition',
+// 		'separate_items_with_commas' => 'Separate recognitions with commas',
+// 		'add_or_remove_items'        => 'Add or remove recognitions',
+// 		'choose_from_most_used'      => 'Choose from the most used',
+// 		'popular_items'              => 'Popular Recognitions',
+// 		'search_items'               => 'Search Recognitions',
+// 		'not_found'                  => 'Not Found',
+// 		'no_terms'                   => 'No Recognitions',
+// 		'items_list'                 => 'Recognitions list',
+// 		'items_list_navigation'      => 'Recognitions list navigation',
+// 	);
+// 	$rewrite = array(
+// 		'slug'                       => 'recognition',
+// 		'with_front'                 => true,
+// 		'hierarchical'               => false,
+// 	);
+// 	$capabilities = array(
+// 		'manage_terms'               => 'manage_options',
+// 		'edit_terms'                 => 'manage_options',
+// 		'delete_terms'               => 'manage_options',
+// 		'assign_terms'               => 'edit_physicians',
+// 	);
+// 	$args = array(
+// 		'labels'                     => $labels,
+// 		'hierarchical'               => true,
+// 		'public'                     => true,
+// 		'show_ui'                    => true,
+// 		'show_admin_column'          => false,
+// 		'show_in_nav_menus'          => false,
+// 		'show_tagcloud'              => false,
+// 		'rewrite'                    => $rewrite,
+// 		'capabilities'               => $capabilities,
+// 	);
+// 	register_taxonomy( 'recognition', array( 'provider' ), $args );
 
-	$labels = array(
-		'name'                       => 'Recognition Lists',
-		'singular_name'              => 'Recognition List',
-		'menu_name'                  => 'Recognition Lists',
-		'all_items'                  => 'All Recognitions',
-		'parent_item'                => 'Parent Recognition',
-		'parent_item_colon'          => 'Parent Recognition:',
-		'new_item_name'              => 'New Recognition',
-		'add_new_item'               => 'Add New Recognition',
-		'edit_item'                  => 'Edit Recognition',
-		'update_item'                => 'Update Recognition',
-		'view_item'                  => 'View Recognition',
-		'separate_items_with_commas' => 'Separate recognitions with commas',
-		'add_or_remove_items'        => 'Add or remove recognitions',
-		'choose_from_most_used'      => 'Choose from the most used',
-		'popular_items'              => 'Popular Recognitions',
-		'search_items'               => 'Search Recognitions',
-		'not_found'                  => 'Not Found',
-		'no_terms'                   => 'No Recognitions',
-		'items_list'                 => 'Recognitions list',
-		'items_list_navigation'      => 'Recognitions list navigation',
-	);
-	$rewrite = array(
-		'slug'                       => 'recognition',
-		'with_front'                 => true,
-		'hierarchical'               => false,
-	);
-	$capabilities = array(
-		'manage_terms'               => 'manage_options',
-		'edit_terms'                 => 'manage_options',
-		'delete_terms'               => 'manage_options',
-		'assign_terms'               => 'edit_physicians',
-	);
-	$args = array(
-		'labels'                     => $labels,
-<<<<<<< Updated upstream
-		'hierarchical'               => true,
-=======
-		'hierarchical'               => false,
->>>>>>> Stashed changes
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => false,
-		'show_in_nav_menus'          => false,
-		'show_tagcloud'              => false,
-		'rewrite'                    => $rewrite,
-		'capabilities'               => $capabilities,
-	);
-	register_taxonomy( 'recognition', array( 'provider' ), $args );
+// }
 
-}
-
-<<<<<<< Updated upstream
-=======
 function create_region_taxonomy() {
 
 	$labels = array(
@@ -1406,8 +1379,6 @@ function create_region_taxonomy() {
 }
 
 /* Custom Roles */
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 function add_roles_on_plugin_activation() {
 	add_role( 'doc_editor', 'Doc Profile Editor',
 			array( 	'read' => true,
