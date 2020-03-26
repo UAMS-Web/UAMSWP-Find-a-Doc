1ac7e7e691bb5f1e425a282a9bd43b6cdd84f37e
Josh <joshdaugherty@gmail.com>
2020-03-26 14:34:02 -0500

    Code refactoring
    Changing shorthand to longhand, etc. More consistent with our other stuff.

--- templates/fwp/location-list.php
+++ templates/fwp/location-list.php
@@ -1,5 +1,5 @@
 <?php 
-if ( have_posts() ) : 
+if ( have_posts() ) { 
     // Generate a table
     ?>
     <table class="table table-striped">
@@ -16,13 +16,16 @@ if ( have_posts() ) :
 
                 $id = get_the_ID();
                 // include( UAMS_FAD_PATH . '/templates/loops/location-card.php' );
-                echo '<tr>';
-                    echo '<th scope="row"><a href="' . get_permalink() . '">'. get_the_title() .'</a></th>';
-                    if (get_field('location_phone', $id)) { ?>
+                ?>
+                <tr>
+                    <th scope="row"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></th>
+                    <?php if (get_field('location_phone', $id)) { ?>
                         <td>
                             <a href="tel:<?php echo format_phone_dash( get_field('location_phone', $id) ); ?>" class="icon-phone"><?php echo format_phone_us( get_field('location_phone', $id) ); ?></a>
                         </td>
-                    <?php } else { echo '<td>&nbsp;</td>'; } ?>
+                    <?php } else { ?>
+                         <td>&nbsp;</td>
+                    <?php } ?>
                     <?php if (get_field('location_new_appointments_phone', $id) && get_field('location_clinic_phone_query', $id)) { ?>
                         <td>
                             <dl>
@@ -31,17 +34,17 @@ if ( have_posts() ) :
                                 <?php if (get_field('location_return_appointments_phone', $id) && get_field('field_location_appointment_phone_query', $id)) { ?>
                                     <dt>Returning Patients</dt>
                                     <dd><a href="tel:<?php echo format_phone_dash( get_field('location_return_appointments_phone', $id) ); ?>" class="icon-phone"><?php echo format_phone_us( get_field('location_return_appointments_phone', $id) ); ?></a></dd>
-                                <?php }
-                            echo '</dl>';
-                        echo '</td>';
-                    } else {
-                        echo '<td>&nbsp;</td>';
-                    }
-                echo '</tr>';
+                                <?php } ?>
+                            </dl>
+                        </td>
+                    <?php } else { ?>
+                        <td>&nbsp;</td>
+                    <?php } ?>
+                </tr>
 
-            endwhile; ?>
+            <?php endwhile; ?>
         </tbody>
     </table>
-<?php else : ?>
+<?php } else { ?>
 	<p><?php _e( 'Sorry, no locations matched your criteria.' ); ?></p>
-<?php endif; ?>
+<?php } //endif ?>
