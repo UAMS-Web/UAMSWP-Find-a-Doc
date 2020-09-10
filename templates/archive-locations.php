b6ccb3318f83844fa229e9f9c8fafe000b1b5787
todd-uams <todd@uams.edu>
2020-09-10 11:09:21 -0500

    Initial Draft of Location Filters based on Provider layout
    Includes Filters for:
     - FWP Search
     - Region
     - AoE
     - Location Type
    
    Filters & pagination are set in FacetWP

--- templates/archive-locations.php
+++ templates/archive-locations.php
@@ -1,7 +1,7 @@
 <?php
-function uamswp_fad_title($html) { 
+function uamswp_fad_title($html) {
 
-	//you can add here all your conditions as if is_page(), is_category() etc.. 
+	//you can add here all your conditions as if is_page(), is_category() etc..
 	$html = 'Locations | ' . get_bloginfo( "name" );
 	return $html;
 }
@@ -13,17 +13,30 @@ get_header();
  ?>
 
 <div class="content-sidebar-wrap">
-    <main id="genesis-content">
-        <section class="archive-description">
-            <header class="entry-header">
-                <h1 class="entry-title" itemprop="headline">Locations</h1>
-            </header>
-        </section>
+    <main id="genesis-content" class="doctor-list">
+        <h1 class="sr-only " itemprop="headline">Locations</h1>
         <section class="uams-module bg-white location-list" id="locations">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-xs-12">
+	                <div class="col-12 col-sm filter-col collapse">
+						<h2 class="h3">Filters</h2>
+						<button type="button" class="close" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<fieldset>
+							<legend class="sr-only">Filter by...</legend>
+							<div class="fwp-filter"><?php echo facetwp_display( 'facet', 'location_search' ); ?></div>
+							<div class="fwp-filter"><?php echo facetwp_display( 'facet', 'location_region' ); ?></div>
+	                        <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'location_aoe' ); ?></div>
+	                        <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'location_type' ); ?></div>
+							<button class="btn btn-primary" id="filter-apply" onclick="FWP.refresh();">Apply</button> <button class="btn btn-outline-primary" id="filter-reset" onclick="FWP.reset()">Reset</button>
+						</fieldset>
+					</div>
+                    <div class="col-12 col-sm list-col">
                         <h2 class="module-title sr-only">List of Locations</h2>
+                        <div>
+
+                        </div>
                         <div class="card-list-container location-card-list-container">
                             <?php echo facetwp_display( 'template', 'locations' ); ?>
                         </div>
