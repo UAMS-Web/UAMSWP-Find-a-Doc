8024d7d158c52fd7b7c0747e121c797ff7080e17
joshdaugherty <joshdaugherty@gmail.com>
2020-10-02 13:05:14 -0500

    Check for parent and add reference to parent in heading

--- templates/loops/expertise-card.php
+++ templates/loops/expertise-card.php
@@ -6,6 +6,25 @@
      *  Must be used inside a loop
      *  Required var: $id
      */
+
+    // Parent Location 
+    $expertise_parent_id = wp_get_post_parent_id($id);
+    $expertise_has_parent = $expertise_parent_id ? true : false;
+    $parent_expertise = '';
+    $parent_id = '';
+    $parent_title = '';
+    $parent_url = '';
+
+    if ($expertise_has_parent && $expertise_parent_id) { 
+        $parent_expertise = get_post( $expertise_parent_id );
+    }
+    // Get Post ID for Address & Image fields
+    if ($parent_expertise) {
+        $parent_id = $parent_expertise->ID;
+        $parent_title = $parent_expertise->post_title;
+        $parent_url = get_permalink( $parent_id );
+    }
+
 ?>
 <div class="card">
     <a href="<?php echo get_permalink($id); ?>" target="_self" aria-label="Go to Area of Expertise page for <?php echo get_the_title($id); ?>">
@@ -22,6 +41,9 @@
     <div class="card-body">
         <h3 class="card-title h5">
             <span class="name"><a href="<?php echo get_permalink($id); ?>" target="_self" aria-label="Go to Area of Expertise page for <?php echo get_the_title($id); ?>"><?php echo get_the_title($id); ?></a></span>
+            <?php if ( $parent_expertise ) { ?>
+                <span class="subtitle"><span class="sr-only">(</span>Part of <a href="<?php echo $parent_url; ?>"><?php echo $parent_title; ?></a><span class="sr-only">)</span></span>
+            <?php } // endif ?>
         </h3>
         <p class="card-text"><?php echo ( $excerpt ? wp_trim_words( $excerpt, 30, ' &hellip;' ) : wp_trim_words( wp_strip_all_tags( get_the_content($id), 30, ' &hellip;' ) ) ); ?></p>
     </div><!-- .card-body -->
