6e6765e2aaa9d18d5b990db08cf3a2e233b38124
Josh <joshdaugherty@gmail.com>
2021-01-27 10:32:05 -0600

    Check for 'none' value in building

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -254,11 +254,11 @@ while ( have_posts() ) : the_post(); ?>
 		$location_floor_label = $location_floor['choices'][ $location_floor_value ];
 	$location_suite = get_field('location_suite', $post_id );
 	$location_address_2 =
-		( $location_building ? $building_name . ( ( ($location_floor && $location_floor_value) || $location_suite ) ? ', ' : '' ) : '' )
+		( ( $location_building && $building_slug != '_none' ) ? $building_name . ( ( ($location_floor && $location_floor_value) || $location_suite ) ? '<br />' : '' ) : '' )
 		. ( $location_floor && !empty($location_floor_value) && $location_floor_value != "0" ? $location_floor_label . ( ( $location_suite ) ? ', ' : '' ) : '' )
 		. ( $location_suite ? $location_suite : '' );
 	$location_address_2_schema =
-		( $location_building ? $building_name . ( ( ($location_floor && $location_floor_value) || $location_suite ) ? ' ' : '' ) : '' )
+		( ( $location_building && $building_slug != '_none' ) ? $building_name . ( ( ($location_floor && $location_floor_value) || $location_suite ) ? ' ' : '' ) : '' )
 		. ( $location_floor && $location_floor_value != "0" ? $location_floor_label . ( ( $location_suite ) ? ' ' : '' ) : '' )
 		. ( $location_suite ? $location_suite : '' );
 
