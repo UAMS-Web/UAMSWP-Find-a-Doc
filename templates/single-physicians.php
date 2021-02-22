d6dddf1722bc3615a44e6b1c893c78074b913620
joshdaugherty <joshdaugherty@gmail.com>
2021-02-22 16:16:45 -0600

    Add a data attribute to the primary location's phone number description list

--- templates/single-physicians.php
+++ templates/single-physicians.php
@@ -345,6 +345,7 @@ while ( have_posts() ) : the_post();
                                 <?php if (get_field('location_phone', $location)) { ?>
                                     <dl>
                                         <dt>Appointment Phone Number<?php echo get_field('field_location_appointment_phone_query', $location) ? 's' : ''; ?></dt>
+                                    <dl data-categorytitle="Telephone Number">
                                         <?php if (get_field('location_new_appointments_phone', $location) && get_field('location_clinic_phone_query', $location)) { ?>
                                             <dd><a href="tel:<?php echo format_phone_dash( get_field('location_new_appointments_phone', $location) ); ?>" class="icon-phone"><?php echo format_phone_us( get_field('location_new_appointments_phone', $location) ); ?></a><?php echo get_field('field_location_appointment_phone_query', $location) ? '<br/><span class="subtitle">New Patients</span>' : '<br/><span class="subtitle">New and Returning Patients</span>'; ?></dd>
                                             <?php if (get_field('location_return_appointments_phone', $location) && get_field('field_location_appointment_phone_query', $location)) { ?>
