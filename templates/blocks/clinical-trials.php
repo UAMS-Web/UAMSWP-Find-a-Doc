4ad805a9559434adda2f7b7eeeeffe39e90b478d
Josh <joshdaugherty@gmail.com>
2021-02-24 11:23:00 -0600

    Add missing ID

--- templates/blocks/clinical-trials.php
+++ templates/blocks/clinical-trials.php
@@ -4,12 +4,12 @@
      *  Designed for UAMS Find-a-Doc
      */
 ?>
-<section class="uams-module cta-bar cta-bar-1 bg-auto">
+<section class="uams-module cta-bar cta-bar-1 bg-auto" id="clinical-trials">
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12">
                 <h2>Clinical Trials</h2>
-                <p><a href="https://uams.trialstoday.org/">Search our clinical trials</a> for those related to <?php echo single_cat_title( '', false ); ?>.</p>
+                <p><a href="https://uams.trialstoday.org/" aria-label="Search UAMS Clinical Trials">Search our clinical trials</a> for those related to <?php echo single_cat_title( '', false ); ?>.</p>
             </div>
         </div>
     </div>
