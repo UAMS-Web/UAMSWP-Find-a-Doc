35b44083f56ee76ca99594bdcb019921d85f22b6
joshdaugherty <joshdaugherty@gmail.com>
2021-05-26 10:35:47 -0500

    Add appointment section

--- templates/single-clinical-resource.php
+++ templates/single-clinical-resource.php
@@ -49,6 +49,7 @@ add_action( 'genesis_after_entry', 'uamswp_resource_treatments_cpt', 14 );
 add_action( 'genesis_after_entry', 'uamswp_resource_physicians', 16 );
 add_action( 'genesis_after_entry', 'uamswp_resource_locations', 18 );
 add_action( 'genesis_after_entry', 'uamswp_resource_expertise', 20 );
+add_action( 'genesis_after_entry', 'uamswp_resource_appointment', 22 );
 
 // Set logic for displaying jump links and sections
 $jump_link_count_min = 2; // How many links have to exist before displaying the list of jump links?
@@ -505,6 +506,29 @@ function uamswp_expertise_jump_links() {
                 </ul>
             </div>
         </nav>
+    <?php }
+}
+function uamswp_resource_appointment() {
+    global $show_appointment_section;
+    
+    if ( $show_appointment_section ) {
+        if ( get_field('location_expertise') ) {
+            $appointment_location_url = '#locations';
+            $appointment_location_label = 'Go to the list of relevant locations';
+        } else {
+            $appointment_location_url = '/location/';
+            $appointment_location_label = 'View a list of UAMS Health locations';
+        } ?>
+        <section class="uams-module cta-bar cta-bar-1 bg-auto" id="appointment-info">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2>Make an Appointment</h2>
+                        <p>Request an appointment by <a href="<?php echo $appointment_location_url; ?>" aria-label="<?php echo $appointment_location_label; ?>" data-itemtitle="Contact a clinic directly">contacting a clinic directly</a> or by calling the UAMS&nbsp;Health appointment line at <a href="tel:501-686-8000" class="no-break" data-itemtitle="Call the UAMS Health appointment line">(501) 686-8000</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     <?php }
 }
 genesis();
