0d25c091f1e53ef664f8e72beed3f2bbd8e8f08a
todd-uams <todd@uams.edu>
2021-05-27 13:42:32 -0500

    Add resource loop for FWP

---
+++ templates/fwp/clinical-resource-loop.php
@@ -0,0 +1,9 @@
+<?php 
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+$id = get_the_ID();
+include( UAMS_FAD_PATH . '/templates/loops/resource-card.php' );
+
+endwhile; else : ?>
+	<p><?php _e( 'Sorry, no resources matched your criteria.' ); ?></p>
+<?php endif; ?>
