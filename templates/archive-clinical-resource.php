8ab73cd49dad65b2e90053301d6ac73cb77a2fd3
todd-uams <todd@uams.edu>
2021-05-27 13:42:53 -0500

    Change archive to match locations

--- templates/archive-clinical-resource.php
+++ templates/archive-clinical-resource.php
@@ -17,50 +17,71 @@ get_header();
  ?>
 
 <div class="content-sidebar-wrap">
-    <main id="genesis-content">
-        <section class="archive-description">
+    <main class="container-fluid doctor-list" id="genesis-content">
+        <h1 class="sr-only" itemprop="headline"><?php echo ($resource_title ? $resource_title : 'Clinical Resources' ); ?></h1>
+        <div class="row">
+            <div class="col-12 col-sm filter-col collapse">
+                <button type="button" class="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h2 class="h4">Search Clinical Resources</h2>
+                <?php  echo do_shortcode( '[wpdreams_ajaxsearchpro id=3]' ); ?>
+                <h2 class="h4">Filters</h2>
+                <fieldset>
+                    <legend class="sr-only">Filter by...</legend>
+                    <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'resource_provider' ); ?></div>
+                    <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'resource_type' ); ?></div>
+                    <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'resource_aoe' ); ?></div>
+                    <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'resource_conditions' ); ?></div>
+                    <div class="fwp-filter"><?php echo facetwp_display( 'facet', 'resource_treatments' ); ?></div>
+                    <button class="btn btn-primary" id="filter-apply" onclick="FWP.refresh();">Apply</button> <button class="btn btn-outline-primary" id="filter-reset" onclick="FWP.reset()">Reset</button>
+                </fieldset>
+            </div>
+        <!-- <section class="archive-description">
             <header class="entry-header">
                 <h1 class="entry-title" itemprop="headline"><?php echo ($resource_title ? $resource_title : 'Clinical Resources' ); ?></h1>
             </header>
             <?php echo ($resource_text ? '<div class="entry-content clearfix" itemprop="text">' . $resource_text . '</div>' : '' ); ?>
-        </section>
-        <section class="uams-module" id="expertise">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <h2 class="module-title sr-only">List of Clinical Resources</h2>
-                        <div class="card-list-container">
-                            <div class="card-list card-list-clinical-resources">
-                                <?php echo facetwp_display( 'template', 'clinical_resources' ); ?>
-                                <?php //get_template_part( 'templates/physician-loop' ); ?>
-                            </div>
-                        </div>
-                        <div class="row list-pagination">
-                            <div class="col">
-                                <?php echo facetwp_display( 'pager' ); ?>
-                            </div>
-                        </div>
+        </section> -->
+            <div class="col-12 col-sm list-col">
+                <h2 class="module-title sr-only">List of Clinical Resources</h2>
+                <div class="row list-col-header">
+                    <div class="col result-count"><?php echo facetwp_display( 'counts' ); ?> Clinical Resources</div>
+                    <div class="col filter-toggle-container">
+                        <!-- When button is active, add "active" class. -->
+                        <button title="Toggle Filter Tray" class="filter-toggle"><span class="sr-only">Toggle Filter Tray</span><span class="fas fa-filter"></span></button>
+                    </div>
+                </div>
+                <div class="card-list-container">
+                    <div class="card-list card-list-clinical-resources">
+                        <?php echo facetwp_display( 'template', 'clinical_resources' ); ?>
+                        <?php //get_template_part( 'templates/physician-loop' ); ?>
+                    </div>
+                </div>
+                <div class="row list-pagination">
+                    <div class="col">
+                        <?php echo facetwp_display( 'pager' ); ?>
                     </div>
                 </div>
             </div>
+        </div> 
             <?php // FacetWP Hide elements
                     // Set # value depending on element
                     ?>
-            <script>
-                (function($) {
-                    $(document).on('facetwp-loaded', function() {
-                        if (3 >= FWP.settings.pager.total_rows ) {
-                            $('.list-pagination').hide()
-                        }
-                        if (FWP.loaded) {
-                            $('html, body').animate({
-                                scrollTop: $('main').offset().top
-                            }, 500);
-                        }
-                    });
-                })(jQuery);
-            </script>
-        </section>
+        <script>
+            (function($) {
+                $(document).on('facetwp-loaded', function() {
+                    if (3 >= FWP.settings.pager.total_rows ) {
+                        $('.list-pagination').hide()
+                    }
+                    if (FWP.loaded) {
+                        $('html, body').animate({
+                            scrollTop: $('main').offset().top
+                        }, 500);
+                    }
+                });
+            })(jQuery);
+        </script>
     </main>
 </div>
 <?php get_footer(); ?>
