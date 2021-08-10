359a748df3b8f70b2a97e3b27afa89ac9f0e885f
todd-uams <todd@uams.edu>
2021-08-10 16:28:16 -0500

    Switch from Cookie to Session

--- templates/archive-locations.php
+++ templates/archive-locations.php
@@ -6,8 +6,8 @@ function uamswp_fad_title($html) {
 	return $html;
 }
 add_filter('pre_get_document_title', 'uamswp_fad_title', 15, 2);
-if (isset( $_COOKIE['_filter_region']) && !isset($_GET['_location_region'])) {
-    $region =  $_COOKIE['_filter_region'];
+if (isset( $_SESSION['_filter_region']) && !isset($_GET['_location_region'])) {
+    $region =  $_SESSION['_filter_region'];
     $url .= $url.(parse_url($url, PHP_URL_QUERY) ? '&' : '?').'_location_region='. $region;
     header("Location: ". $url);
     exit();
