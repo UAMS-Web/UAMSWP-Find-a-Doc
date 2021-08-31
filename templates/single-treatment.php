90a0eadca3059b1cae34476d2641f3a4f5d755ec
joshdaugherty <joshdaugherty@gmail.com>
2021-08-31 11:53:39 -0500

    Code refactoring: indent

--- templates/single-treatment.php
+++ templates/single-treatment.php
@@ -650,9 +650,9 @@
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-12">
-						<h2 class="module-title"><span class="title">Providers Performing <?php echo $page_title; ?></span></h2>
-						<p class="note">Note that every provider listed below may not perform or prescribe <?php echo $page_title; ?> for all conditions related to it. Review each provider for availability.</p>   
-						<?php echo do_shortcode( '[uamswp_provider_ajax_filter providers="'. implode(",", $provider_ids) .'"]' ); ?>
+							<h2 class="module-title"><span class="title">Providers Performing <?php echo $page_title; ?></span></h2>
+							<p class="note">Note that every provider listed below may not perform or prescribe <?php echo $page_title; ?> for all conditions related to it. Review each provider for availability.</p>   
+							<?php echo do_shortcode( '[uamswp_provider_ajax_filter providers="'. implode(",", $provider_ids) .'"]' ); ?>
 							<div class="card-list-container">
 								<div class="card-list card-list-doctors card-list-doctors-count-<?php echo $postsCountClass; ?>">
 								<?php 
