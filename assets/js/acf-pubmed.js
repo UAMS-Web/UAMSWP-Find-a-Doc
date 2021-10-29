efd5bacaaec8735020d8d77e1cb01120797e0010
todd-uams <todd@uams.edu>
2021-10-29 10:47:16 -0500

    Rewrite the JS to make function available to "on change" & button "click"

--- assets/js/acf-pubmed.js
+++ assets/js/acf-pubmed.js
@@ -4,23 +4,31 @@ jQuery(document).ready(function($){
     if (typeof acf == 'undefined') { return; }
     
     $(document).on('change', '[data-key="field_physician_select_publications_pmid"] .acf-input input', function(e) {
-        // we are not going to do anyting in this anonymous function
-        // the reason is explained below
-        // call function, we need to pass the event and jQuery object
-        update_pubmed_on_state_change(e, $);
+        // we are not going to get the id of the input field
+        // then retrieve the value to pass into the updated function
+        var input = e.target.id;
+        var value = document.getElementById(input).value;
+        // console.log(input); // In case we need to test
+        update_pubmed_on_state_change( input, value );
+    });
+    $(document).on('click', '[data-key="field_physician_select_publications_lookup"] .acf-input button', function(e) {
+        // we are not going to get the id of the input field of the pmid
+        // then retrieve the value to pass into the updated function
+        pubmed_lookup_name = $(e.target).attr('name'); // get "name" of the button
+        pubmed_lookup_pmid = pubmed_lookup_name.replace('field_physician_select_publications_lookup', 'field_physician_select_publications_pmid');
+        var input = $("input[name='" + pubmed_lookup_pmid +"']").attr('id');    // #id of pmid input
+        var value = $("input[name='" + pubmed_lookup_pmid +"']").val();         // value of the pmid input
+        // console.log( input ); // In case we need to test
+        update_pubmed_on_state_change( input, value );
     });
     $('[data-key="field_physician_select_publications_pmid"] .acf-input input').trigger('ready');
 });
     
-function update_pubmed_on_state_change(e, $) {
+function update_pubmed_on_state_change(input, value) {
     if (this.request) {
         // if a recent request has been made abort it
         this.request.abort();
     }
-
-    // get the target of the event and then get the value of that field
-    var input = $(e.target);
-    var value = input.val();
 
     if (!value) {
         // no state selected
@@ -28,12 +36,11 @@ function update_pubmed_on_state_change(e, $) {
         return;
     }
     
-    // get the city select field, and remove all exisiting choices
-    var pubmed_textarea = input.attr('id').replace('field_physician_select_publications_pmid', 'field_physician_select_publications_pubmed');
-    pubmed_textarea = $('textarea#' +pubmed_textarea); // Set textarea id
-    // console.log(pubmed_textarea);
-    pubmed_textarea.val('Loading ...'); // Clear the value
-    pubmed_textarea.attr('readonly', true);
+    // replace the pmid id with pubmed (textarea) id
+    var pubmed_textarea = input.replace('field_physician_select_publications_pmid', 'field_physician_select_publications_pubmed');
+    // console.log(pubmed_textarea); // In case we need to test
+    document.getElementById(pubmed_textarea).value = 'Loading ...'; // Clear the value
+    document.getElementById(pubmed_textarea).readonly = true; // Make sure the field is readonly
     
     // set and prepare data for ajax
     var data = {
@@ -49,7 +56,7 @@ function update_pubmed_on_state_change(e, $) {
     // instead of going through the acf.ajax object to make requests like in <5.7
     // we need to do a lot of the work ourselves, but other than the method that's called
     // this has not changed much
-    this.request = $.ajax({
+    this.request = jQuery.ajax({
         url: acf.get('ajaxurl'), // acf stored value
         data: data,
         type: 'post',
@@ -61,116 +68,19 @@ function update_pubmed_on_state_change(e, $) {
 
             console.log('ajax success');
 
-            //console.log(json);
+            console.log(json['full']);
 
             // put the values into the fields
             if (json['full']) {
-                // data-key == field key of textarea field
-                $( pubmed_textarea ).val(json['full']);
-                $( pubmed_textarea ).attr('readonly', true);
+                // replace the textarea text with ajax results
+                document.getElementById(pubmed_textarea).value = json['full'];
+                document.getElementById(pubmed_textarea).readonly = true;
             }
         },
         error: function(jqXHR, textStatus, error) {
+            document.getElementById(pubmed_textarea).value; // remove the value from the textarea
             alert (jqXHR+' : '+textStatus+' : '+error);
         }
     });
     
 }
-// Original JS below
-// jQuery(document).ready(function($){
-// 	// make sure acf is loaded, it should be, but just in case
-// 	if (typeof acf == 'undefined') { return; }
-
-// 	// extend the acf.ajax object
-// 	// you should probably rename this var
-// 	var pubmedExtension = acf.Model.extend({
-// 		events: {
-// 			// this data-key must match the field key for the input field that
-// 			// will trigger the change of the other fields. In this case, the input field
-// 			// is a text field and we need to set an event and get the value.
-// 			// When ACF updates the value it triggers a change action on a different t
-// 			// extarea field than the one that will contain the value
-// 			'change [data-key="field_physician_select_publications_pmid"] .acf-input input': '_text_change',
-// 		},
-
-// 		_text_change: function(e) {
-
-// 			// get which input changed
-// 			var $input = e.$el;
-// 			// create the target id
-// 			var $target = $input.attr('id').replace('field_physician_select_publications_pmid', 'field_physician_select_publications_pubmed');
-
-// 			//console.log($input);
-// 			//console.log('textarea#' +$target);
-
-// 			// clear existing values from the fields we will update
-// 			// clear input field
-// 			// targer == field key of textarea field
-// 			$( 'textarea#' +$target ).val('');
-
-// 			// get the input value
-// 			var $value = e.$el.val();
-
-// 			// if there is no value, exit
-// 			if (!$value) {
-// 				return;
-// 			}
-
-// 			//console.log('started ' + $value);
-
-// 			// now we can do our ajax request
-
-// 			// I assume this tests to see if there is already a request
-// 			// for this and cancels it if there is
-// 			if( this.request) {
-// 				this.request.abort();
-// 			}
-
-// 			// I don't know exactly what it does
-// 			// acf does it so I copied it
-// 			var self = this,
-// 			    data = this.o;
-
-// 			// set the ajax action that's set up in php
-// 			data.action = 'load_content_from_pubmed';
-// 			// set the term id value to be submitted
-// 			data.pmid = $value;
-
-// 			// this is another bit I'm not sure about
-// 			// copied from ACF
-// 			data.exists = [];
-
-// 			//console.log( acf.get('ajaxurl') );
-
-// 			// this the request is copied from ACF
-// 			this.request = $.ajax({
-// 				url:		acf.get('ajaxurl'),
-// 				data:		acf.prepareForAjax(data),
-// 				type:		'post',
-// 				dataType:	'json',
-// 				async: true,
-// 				success: function(json){
-
-// 					if (!json) {
-// 						return;
-// 					}
-
-// 					console.log('ajax success');
-
-// 					//console.log(json);
-
-// 					// put the values into the fields
-// 					if (json['full']) {
-// 						// data-key == field key of textarea field
-// 						// originally '[data-key="field_592437c881851"] textarea'
-// 						$( 'textarea#' +$target ).val(json['full']);
-// 					}
-// 				},
-// 				error: function(jqXHR, textStatus, error) {
-// 					alert (jqXHR+' : '+textStatus+' : '+error);
-// 				}
-// 			});
-// 		},
-// 	});
-
-// });
