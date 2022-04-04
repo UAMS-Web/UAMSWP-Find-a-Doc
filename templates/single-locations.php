7eba0ee850ce8460c1d4d1a1d4fefb960593d671
joshdaugherty <joshdaugherty@gmail.com>
2022-04-04 09:51:32 -0500

    [FD20-520] Move address and contact info into columns

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -624,40 +624,47 @@ while ( have_posts() ) : the_post(); ?>
 							<?php } ?>
 						</div>
 					<?php } // endif ?>
-					<h2 class="sr-only">Address</h2>
-					<p><?php echo $location_address_1; ?><br/>
-					<?php echo ( $location_address_2 ? $location_address_2 . '<br/>' : ( $location_address_2_deprecated ? $location_address_2_deprecated . '<br/>' : '')); ?>
-					<?php echo $location_city; ?>, <?php echo $location_state; ?> <?php echo $location_zip; ?></p>
-						<div class="btn-container">
-							<div class="inner-container">
-								<a class="btn btn-primary" href="https://www.google.com/maps/dir/Current+Location/<?php echo $map['lat'] ?>,<?php echo $map['lng'] ?>" target="_blank" aria-label="Get directions to <?php echo $page_title; ?>" data-typetitle="Get directions to the clinic">Get Directions</a>
-								<?php if ($show_parking_section) { ?>
-									<a class="btn btn-outline-primary" href="#parking-info" aria-label="Parking instructions for <?php echo $page_title; ?>" data-typetitle="Parking instructions for the clinic">Parking Instructions</a>
-								<?php } // endif $show_parking_section ?>
-								
-							</div>
+					<div class="row">
+						<div class="col-lg">
+							<h2 class="sr-only">Address</h2>
+							<p><?php echo $location_address_1; ?><br/>
+							<?php echo ( $location_address_2 ? $location_address_2 . '<br/>' : ( $location_address_2_deprecated ? $location_address_2_deprecated . '<br/>' : '')); ?>
+							<?php echo $location_city; ?>, <?php echo $location_state; ?> <?php echo $location_zip; ?></p>
+								<div class="btn-container">
+									<div class="inner-container">
+										<a class="btn btn-primary" href="https://www.google.com/maps/dir/Current+Location/<?php echo $map['lat'] ?>,<?php echo $map['lng'] ?>" target="_blank" aria-label="Get directions to <?php echo $page_title; ?>" data-typetitle="Get directions to the clinic">Get Directions</a>
+										<?php if ($show_parking_section) { ?>
+											<a class="btn btn-outline-primary" href="#parking-info" aria-label="Parking instructions for <?php echo $page_title; ?>" data-typetitle="Parking instructions for the clinic">Parking Instructions</a>
+										<?php } // endif $show_parking_section ?>
+										
+									</div>
+								</div>
+								<?php if( $location_web_name && $location_url ){ ?>
+									<p><a class="btn btn-secondary" href="<?php echo $location_url['url']; ?>" target="_blank" data-categorytitle="External Link"><?php echo $location_web_name; ?> <span class="far fa-external-link-alt"></span></span></a></p>
+							<?php } 
+								// Schema data
+								$location_schema = '"address": {
+								"@type": "PostalAddress",
+								"streetAddress": "'. $location_address_1 . ' '. $location_address_2_schema .'",
+								"addressLocality": "'. $location_city .'",
+								"addressRegion": "'. $location_state .'",
+								"postalCode": "'. $location_zip .'"
+								},
+								';
+								$phone_schema = '';
+							?>
 						</div>
-						<?php if( $location_web_name && $location_url ){ ?>
-							<p><a class="btn btn-secondary" href="<?php echo $location_url['url']; ?>" target="_blank" data-categorytitle="External Link"><?php echo $location_web_name; ?> <span class="far fa-external-link-alt"></span></span></a></p>
-					<?php } 
-						// Schema data
-						$location_schema = '"address": {
-						"@type": "PostalAddress",
-						"streetAddress": "'. $location_address_1 . ' '. $location_address_2_schema .'",
-						"addressLocality": "'. $location_city .'",
-						"addressRegion": "'. $location_state .'",
-						"postalCode": "'. $location_zip .'"
-						},
-						';
-						$phone_schema = '';
-					?>
-					<h2>Contact Information</h2>
+						<div class="col-lg">
+							<h2>Contact Information</h2>
+							<?php
+								// Phone values
+								$phone_output_id = $id;
+								$phone_output = 'location_profile';
+								include( UAMS_FAD_PATH . '/templates/blocks/locations-phone.php' );
+							?>
+						</div>
+					</div>
 					<?php 
-					// Phone values
-					$phone_output_id = $id;
-					$phone_output = 'location_profile';
-					include( UAMS_FAD_PATH . '/templates/blocks/locations-phone.php' );
-					
 					// Hours values
 					$hoursvary = $location_hours_group['location_hours_variable'];
 					$hoursvary_info = $location_hours_group['location_hours_variable_info'];
