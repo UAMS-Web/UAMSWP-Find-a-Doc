0abc45173761d5fb164109cfc77761d364fb35e5
joshdaugherty <joshdaugherty@gmail.com>
2022-04-06 09:21:37 -0500

    [FD20-520] Adjust variable names for reusability

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -406,27 +406,27 @@ while ( have_posts() ) : the_post(); ?>
         }
 
 		// Check if specialty care appointment request link should be displayed
-		$location_appt_request_query = get_field('location_appt_request_query');
-		$location_appt_request_forms = get_field('location_appt_request_form');
+		$appointment_request_query = get_field('location_appt_request_query');
+		$appointment_request_forms = get_field('location_appt_request_form');
 		// Check for valid forms
-		$location_appt_request_form_valid = false;
-		if ( $location_appt_request_query && $location_appt_request_forms ) {
-			foreach( $location_appt_request_forms as $form ) {
+		$appointment_request_form_valid = false;
+		if ( $appointment_request_query && $appointment_request_forms ) {
+			foreach( $appointment_request_forms as $form ) {
 				$form_object = get_term_by( 'id', $form, 'appointment_request');
 				if ( $form_object ) {
-					$location_appt_request_form_valid = true;
+					$appointment_request_form_valid = true;
 					$break;
 				}
 			}
 		}
-		if ( $location_appt_request_form_valid ) {
-			$show_appt_request_section = true;
+		if ( $appointment_request_form_valid ) {
+			$show_appointment_request_section = true;
 		} else {
-			$show_appt_request_section = false;
+			$show_appointment_request_section = false;
 		}
 
 		// Check if online appointment scheduling/request section should be displayed in the top section
-		if ( $show_mychart_scheduling_section || $show_appt_request_section ) {
+		if ( $show_mychart_scheduling_section || $show_appointment_request_section ) {
             $show_online_scheduling_section = true;
         } else {
             $show_online_scheduling_section = false;
@@ -993,30 +993,30 @@ while ( have_posts() ) : the_post(); ?>
 								// End MyChart Scheduling Links Section
 								// Begin link to specialized care appointment request
 
-								if ( $show_appt_request_section ) {
-									$location_appt_request_form_count = count($location_appt_request_forms);
+								if ( $show_appointment_request_section ) {
+									$appointment_request_form_count = count($appointment_request_forms);
 
-									$location_appt_request_heading = 'Specialized Care';
-									$location_appt_request_heading_standalone = $appointments_heading;
-									$location_appt_request_intro = 'Some appointments for specialized care at this location cannot be scheduled online. For those, submit a request for an appointment.';
-									$location_appt_request_intro_standalone = 'Appointments for specialized care at this location cannot be scheduled online. For those, submit a request for an appointment.';
-									$location_appt_request_button_text = 'Request an Appointment';
+									$appointment_request_heading = 'Specialized Care';
+									$appointment_request_heading_standalone = $appointments_heading;
+									$appointment_request_intro = 'Some appointments for specialized care at this location cannot be scheduled online. For those, submit a request for an appointment.';
+									$appointment_request_intro_standalone = 'Appointments for specialized care at this location cannot be scheduled online. For those, submit a request for an appointment.';
+									$appointment_request_button_text = 'Request an Appointment';
 									
 									if ( $show_mychart_scheduling_section ) {
-										echo '<h3 class="h5">' . $location_appt_request_heading . '</h3>';
-										echo '<p>' . $location_appt_request_intro . '</p>';
+										echo '<h3 class="h5">' . $appointment_request_heading . '</h3>';
+										echo '<p>' . $appointment_request_intro . '</p>';
 									} else {
-										echo '<h2 class="h4">' . $location_appt_request_heading_standalone . '</h2>';
-										echo '<p>' . $location_appt_request_intro_standalone . '</p>';
+										echo '<h2 class="h4">' . $appointment_request_heading_standalone . '</h2>';
+										echo '<p>' . $appointment_request_intro_standalone . '</p>';
 									} ?>
 									<div class="btn-container">
 										<div class="inner-container">
 											<?php
-											if ( $location_appt_request_form_count > 1 ) { ?>
+											if ( $appointment_request_form_count > 1 ) { ?>
 												<div class="dropdown">
-													<button class="btn btn-outline-primary dropdown-toggle" type="button" id="appt_request_form_dropdown" data-toggle="dropdown" aria-expanded="false"><?php echo $location_appt_request_button_text; ?></button>
+													<button class="btn btn-outline-primary dropdown-toggle" type="button" id="appt_request_form_dropdown" data-toggle="dropdown" aria-expanded="false"><?php echo $appointment_request_button_text; ?></button>
 													<div class="dropdown-menu" aria-labelledby="appt_request_form_dropdown">
-														<?php foreach( $location_appt_request_forms as $form ) {
+														<?php foreach( $appointment_request_forms as $form ) {
 															$form_object = get_term_by( 'id', $form, 'appointment_request');
 															if ( $form_object ) {
 																$form_object_name = $form_object->name;
@@ -1030,7 +1030,7 @@ while ( have_posts() ) : the_post(); ?>
 													</div>
 												</div>
 											<?php } else {
-												foreach( $location_appt_request_forms as $form ) {
+												foreach( $appointment_request_forms as $form ) {
 													$form_object = get_term_by( 'id', $form, 'appointment_request');
 													if ( $form_object ) {
 														$form_object_name = $form_object->name;
@@ -1038,7 +1038,7 @@ while ( have_posts() ) : the_post(); ?>
 														$form_object_name_attr = html_entity_decode(str_replace('&nbsp;', ' ', htmlentities($form_object_name_attr, null, 'utf-8')));
 														$form_url = get_field('appointment_request_url', $form_object);
 														?>
-														<a class="btn btn-outline-primary" href="<?php echo $form_url; ?>" target="_blank" aria-label="<?php echo $location_appt_request_button_text . ', ' . $form_object_name_attr; ?>"><?php echo $location_appt_request_button_text; ?></a>
+														<a class="btn btn-outline-primary" href="<?php echo $form_url; ?>" target="_blank" aria-label="<?php echo $appointment_request_button_text . ', ' . $form_object_name_attr; ?>"><?php echo $appointment_request_button_text; ?></a>
 													<?php }
 												}
 											} ?>
