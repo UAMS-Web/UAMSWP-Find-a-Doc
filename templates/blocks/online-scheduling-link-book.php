0b00a50ad8afda2a8dc15b559ab93628ed294ccf
Josh <joshdaugherty@gmail.com>
2022-04-06 17:03:21 -0500

    [FD20-520] Switch from while(have_rows()) to foreach()
    Passing the have_rows() variable into the template was being problematic.

--- templates/blocks/online-scheduling-link-book.php
+++ templates/blocks/online-scheduling-link-book.php
@@ -6,7 +6,7 @@
      *  Required vars:
      * 
      *  Optional vars:
-     *      $mychart_scheduling_options_rows
+     *      $mychart_scheduling_options
      */
 ?>
 <div class="btn-container">
@@ -16,22 +16,22 @@
             <div class="dropdown-menu" aria-labelledby="mychart_scheduling_dropdown">
                 <?php 
                 $i = 0;
-                // Loop through rows.
-                while( $mychart_scheduling_options_rows ) {
-                    the_row();
-                    // Load sub field value.
-                    $visit_type = get_sub_field('location_scheduling_vt');
-                    $visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type');
+                // Loop through repeater rows.
+                if ( $mychart_scheduling_options ) {
+                    foreach( $mychart_scheduling_options as $option ) {
+                        // Load sub field value.
+                        $visit_type = $option['location_scheduling_vt'];
+                        $visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type');
 
-                    // Do something...
-                    if ( $visit_type_object ) {
-                        $visit_type_link_text = get_field('mychart_visit_type_link_text', $visit_type_object);
-                        ?>
-                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $visit_type_link_text; ?></a>
-                    <?php }
-                    $i++;
-
-                } // endwhile (have_rows) ?>
+                        // Do something...
+                        if ( $visit_type_object ) {
+                            $visit_type_link_text = get_field('mychart_visit_type_link_text', $visit_type_object);
+                            ?>
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $visit_type_link_text; ?></a>
+                        <?php }
+                        $i++;
+                    } // end foreach
+                } // endif $mychart_scheduling_options ?>
             </div>
         </div>
     </div>
