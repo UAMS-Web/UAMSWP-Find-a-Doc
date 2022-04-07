feb1e219c6e8b63a03cea8df6e27ab134f6aa178
Josh <joshdaugherty@gmail.com>
2022-04-07 10:09:51 -0500

    [FD20-520] Replace appointment request link with block
    ... on single provider template

--- templates/single-physicians.php
+++ templates/single-physicians.php
@@ -536,42 +536,8 @@ while ( have_posts() ) : the_post();
                                             echo '<h2 class="h4">' . $appointment_request_heading_standalone . '</h2>';
                                             echo '<p>' . $appointment_request_intro_standalone . '</p>';
                                         }
+									    include( UAMS_FAD_PATH . '/templates/blocks/online-scheduling-link-request.php' );
                                         ?>
-                                        <div class="btn-container">
-                                            <div class="inner-container">
-                                                <?php
-                                                if ( $appointment_request_form_count > 1 ) { ?>
-                                                    <div class="dropdown">
-                                                        <button class="btn btn-outline-primary dropdown-toggle" type="button" id="appt_request_form_dropdown" data-toggle="dropdown" aria-expanded="false"><?php echo $appointment_request_button_text; ?></button>
-                                                        <div class="dropdown-menu" aria-labelledby="appt_request_form_dropdown">
-                                                            <?php foreach( $appointment_request_forms as $form ) {
-                                                                $form_object = get_term_by( 'id', $form, 'appointment_request');
-                                                                if ( $form_object ) {
-                                                                    $form_object_name = $form_object->name;
-                                                                    $form_object_name_attr = str_replace('"', '\'', $form_object_name);
-                                                                    $form_object_name_attr = html_entity_decode(str_replace('&nbsp;', ' ', htmlentities($form_object_name_attr, null, 'utf-8')));
-                                                                    $form_url = get_field('appointment_request_url', $form_object);
-                                                                    ?>
-                                                                    <a class="dropdown-item" href="<?php echo $form_url; ?>" target="_blank"><?php echo $form_object_name; ?></a>
-                                                                <?php }
-                                                            } ?>
-                                                        </div>
-                                                    </div>
-                                                <?php } else {
-                                                    foreach( $appointment_request_forms as $form ) {
-                                                        $form_object = get_term_by( 'id', $form, 'appointment_request');
-                                                        if ( $form_object ) {
-                                                            $form_object_name = $form_object->name;
-                                                            $form_object_name_attr = str_replace('"', '\'', $form_object_name);
-                                                            $form_object_name_attr = html_entity_decode(str_replace('&nbsp;', ' ', htmlentities($form_object_name_attr, null, 'utf-8')));
-                                                            $form_url = get_field('appointment_request_url', $form_object);
-                                                            ?>
-                                                            <a class="btn btn-outline-primary" href="<?php echo $form_url; ?>" target="_blank" aria-label="<?php echo $appointment_request_button_text . ', ' . $form_object_name_attr; ?>"><?php echo $appointment_request_button_text; ?></a>
-                                                        <?php }
-                                                    }
-                                                } ?>
-                                            </div>
-                                        </div>
                                     </div>
                                 <?php } // endif $show_appointment_request_section
                                 // End link to specialized care appointment request ?>
