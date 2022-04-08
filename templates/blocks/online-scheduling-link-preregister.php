bdc6053e23a2b004762a4e2ce8a032f5a35ba5d9
Josh <joshdaugherty@gmail.com>
2022-04-08 16:57:16 -0500

    [FD20-520] Create visit pre-register link bock

---
+++ templates/blocks/online-scheduling-link-preregister.php
@@ -0,0 +1,48 @@
+<?php
+    /**
+     *  Template Name: Online Scheduling, MyChart Open Scheduling Widget Modal Toggles
+     *  Designed for UAMS Find-a-Doc
+     * 
+     *  Required vars:
+     * 
+     *  Optional vars from single location template:
+     *      $mychart_scheduling_preregister_options
+     * 
+     *  Optional vars from single provider template:
+     *      $mychart_scheduling_visit_type
+     */
+
+// Check optional vars from single location template
+$mychart_scheduling_preregister_options = isset($mychart_scheduling_preregister_options) ? $mychart_scheduling_preregister_options : '';
+
+// Check optional vars from single provider template
+$mychart_scheduling_visit_type = isset($mychart_scheduling_visit_type) ? $mychart_scheduling_visit_type : '';
+?>
+<div class="btn-container">
+    <div class="inner-container">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="mychart_scheduling_dropdown" data-toggle="dropdown" aria-expanded="false">Pre-register</button>
+            <div class="dropdown-menu" aria-labelledby="mychart_scheduling_dropdown">
+                <?php 
+                $i = 0;
+                // Loop through repeater rows.
+                if ( $mychart_scheduling_preregister_options || $mychart_scheduling_visit_type) {
+                    $options = $mychart_scheduling_preregister_options ?: $mychart_scheduling_visit_type;
+                    foreach( $options as $option ) {
+                        // Load sub field value.
+                        $visit_type = $option['location_scheduling_vt'] ?: $option;
+                        $visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type');
+
+                        // Do something...
+                        if ( $visit_type_object ) {
+                            $visit_type_link_text = get_field('mychart_visit_type_link_text', $visit_type_object);
+                            ?>
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $visit_type_link_text; ?></a>
+                        <?php }
+                        $i++;
+                    } // end foreach
+                } // endif ( $mychart_scheduling_preregister_options || $mychart_scheduling_visit_type) ?>
+            </div>
+        </div>
+    </div>
+</div>
