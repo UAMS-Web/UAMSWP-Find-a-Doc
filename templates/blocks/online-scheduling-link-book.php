cbc91c985d26125cbcfb2caf14633662ebdccc61
Josh <joshdaugherty@gmail.com>
2023-05-18 15:44:19 -0500

    [FD20-520] Make booking button dropdown conditional

--- templates/blocks/online-scheduling-link-book.php
+++ templates/blocks/online-scheduling-link-book.php
@@ -20,39 +20,90 @@
  * 	
  */
 
-// Set the variable that does not exist in the single provider template
-if ( $scheduling_template == 'single-provider' ) {
+// Count visit types
+$scheduling_mychart_book_options = isset($scheduling_mychart_book_options) ? $scheduling_mychart_book_options : '';
+$scheduling_mychart_book_visit_type = isset($scheduling_mychart_book_visit_type) ? $scheduling_mychart_book_visit_type : '';
+$scheduling_mychart_book_count = '';
+if ( $scheduling_mychart_book_options ) {
+	$scheduling_mychart_book_count = count( $scheduling_mychart_book_options );
+} elseif ( $scheduling_mychart_book_visit_type ) {
+	$scheduling_mychart_book_count = count( $scheduling_mychart_book_visit_type );
+}
+
+// Set the dropdown query variable
+if (
+	$scheduling_template == 'single-provider' // If it is on the single provider template
+	||
+	$scheduling_mychart_book_count > 1 // If there is more than one visit type selected
+) {
 	$scheduling_mychart_book_dropdown = true;
 } else {
 	$scheduling_mychart_book_dropdown = isset($scheduling_mychart_book_dropdown) ? $scheduling_mychart_book_dropdown : true;
 }
 
+// Create the dropdown element
+$scheduling_mychart_book_btn_text = 'Book an Appointment';
 if (
-	$scheduling_mychart_book_options // MyChart open scheduling widget options for Appointment Booking is not empty
+	$scheduling_mychart_book_options // Single Location: MyChart open scheduling widget options for Appointment Booking is not empty
 	||
-	$scheduling_mychart_book_visit_type // At least one Visit Type has been selected
+	$scheduling_mychart_book_visit_type // Single Provider: At least one Visit Type has been selected
 ) {
-?>
-<div class="dropdown">
-	<button class="btn btn-primary dropdown-toggle" type="button" id="mychart_scheduling_dropdown" data-toggle="dropdown" aria-expanded="false">Book an Appointment</button>
-	<div class="dropdown-menu" aria-labelledby="mychart_scheduling_dropdown">
-		<?php 
+	if ( $scheduling_mychart_book_dropdown ) { // If dropdown should be displayed ?>
+		<div class="dropdown">
+			<button class="btn btn-primary dropdown-toggle" type="button" id="mychart_scheduling_dropdown" data-toggle="dropdown" aria-expanded="false"><?php echo $scheduling_mychart_book_btn_text; ?></button>
+			<div class="dropdown-menu" aria-labelledby="mychart_scheduling_dropdown">
+				<?php 
+
+				// Loop through visit type repeater rows
+
+				$i = 0;
+				$options = $scheduling_mychart_book_options ?: $scheduling_mychart_book_visit_type;
+				foreach ( $options as $option ) {
+					// Load sub field value.
+					$visit_type = $option['location_scheduling_vt'] ?: $option; // Get ID of location's visit type. Otherwise, get ID of provider's visit type.
+					$visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type'); // Get visit type taxonomy object based on the selected ID
+					
+					// Do something...
+					if ( $visit_type_object ) { // If the object exists...
+						// Get Open Scheduling Widget Link Text
+						$visit_type_link_text = get_field('mychart_visit_type_link_text', $visit_type_object);
+
+						// Create the modal link
+						?>
+						<a class="dropdown-item" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $visit_type_link_text; ?></a>
+					<?php } // endif ( $visit_type_object )
+					$i++;
+				} // end foreach ( $options as $option )
+
+				// End looping through visit type repeater rows
+				
+				?>
+			</div>
+		</div>
+	<?php } else { // Otherwise, no dropdown should be displayed
+		
+		// Loop through visit type repeater rows
+
 		$i = 0;
-		// Loop through repeater rows.
 		$options = $scheduling_mychart_book_options ?: $scheduling_mychart_book_visit_type;
-		foreach( $options as $option ) {
+		foreach ( $options as $option ) {
 			// Load sub field value.
-			$visit_type = $option['location_scheduling_vt'] ?: $option;
-			$visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type');
+			$visit_type = $option['location_scheduling_vt'] ?: $option; // Get ID of location's visit type. Otherwise, get ID of provider's visit type.
+			$visit_type_object = get_term_by( 'id', $visit_type, 'mychart_visit_type'); // Get visit type taxonomy object based on the selected ID
 			
 			// Do something...
-			if ( $visit_type_object ) {
+			if ( $visit_type_object ) { // If the object exists...
+				// Get Open Scheduling Widget Link Text
 				$visit_type_link_text = get_field('mychart_visit_type_link_text', $visit_type_object);
+
+				// Create the modal link
 				?>
-				<a class="dropdown-item" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $visit_type_link_text; ?></a>
-			<?php }
+				<a class="btn btn-primary" href="#" data-toggle="modal" data-target="#mychart-scheduling_<?php echo $i; ?>"><?php echo $scheduling_mychart_book_btn_text; ?></a>
+			<?php } // endif ( $visit_type_object )
 			$i++;
-		} // end foreach ?>
-	</div>
-</div>
-<?php } // endif ?>
+		} // end foreach ( $options as $option )
+
+		// End looping through visit type repeater rows
+
+	} // endif ( $scheduling_mychart_book_dropdown ) else
+} // endif ( $scheduling_mychart_book_options || $scheduling_mychart_book_visit_type) ?>
