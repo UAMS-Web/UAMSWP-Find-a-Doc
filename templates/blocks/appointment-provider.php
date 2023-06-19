5292b5f26665370e202869397b7594fb39ca10d8
Josh <joshdaugherty@gmail.com>
2023-06-19 09:17:21 -0500

    [FD20-3261] Change $show_portal var name
    $show_portal -> $portal_section_show

--- templates/blocks/appointment-provider.php
+++ templates/blocks/appointment-provider.php
@@ -14,7 +14,7 @@ $appointment_show_main_line_sys = get_field('provider_mainline', 'option');
 
 $appointment_phone_name = 'the UAMS&nbsp;Health appointment line'; // default (UAMS)
 $appointment_phone = '5016868000'; // default (UAMS)
-$show_portal = false;
+$portal_section_show = false;
 // Portal
 if ( $physician_portal ) {
 	$portal = get_term($physician_portal, "portal");
@@ -29,7 +29,7 @@ if ( $physician_portal ) {
 	}
 
 	if ($location_valid && $portal && $portal_slug !== "_none") {
-		$show_portal = true;
+		$portal_section_show = true;
 	}
 	if ($portal_slug == "ach-mychart") {
 		$appointment_phone_name = 'the main Arkansas Children\'s Hospital appointment line';
@@ -64,14 +64,14 @@ $appointment_reference_main = 'by calling ' . $appointment_phone_name . ' at <a 
 		<div class="row">
 			<div class="col-xs-12">
 				<h2>Make an Appointment With <?php echo $short_name; ?></h2>
-				<?php if ($location_valid && $refer_req && $accept_new && $show_portal) { ?>
+				<?php if ($location_valid && $refer_req && $accept_new && $portal_section_show) { ?>
 					<p><?php echo $appointment_reference_referral; ?></p>
 					<p>Existing patients can make an appointment by <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_portal; ?>, <?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.
 					<?php } else { ?>
 						<?php echo $appointment_reference_portal; ?> or <?php echo $appointment_reference_direct; ?>.
 					<?php } ?></p>
-				<?php } elseif (!$location_valid && $refer_req && $accept_new && !$show_portal) {
+				<?php } elseif (!$location_valid && $refer_req && $accept_new && !$portal_section_show) {
 					// Showing main appointment line reference here regardless of 
 					// decision on main appointment line, as there is no valid location 
 					// in this scenario. There would otherwise be no manner to make an 
@@ -79,41 +79,41 @@ $appointment_reference_main = 'by calling ' . $appointment_phone_name . ' at <a 
 				?>
 					<p><?php echo $appointment_reference_referral; ?></p>
 					<p>Existing patients can make an appointment <?php echo $appointment_reference_main; ?>.</p>
-				<?php } elseif ($location_valid && $refer_req && $accept_new && !$show_portal) { ?>
+				<?php } elseif ($location_valid && $refer_req && $accept_new && !$portal_section_show) { ?>
 					<p><?php echo $appointment_reference_referral; ?></p>
 					<p>Existing patients can make an appointment <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.
 					<? } else { ?>
 						<?php echo $appointment_reference_direct; ?>.
 					<?php } ?></p>
-				<?php } elseif ($location_valid && !$refer_req && $accept_new && $show_portal) { ?>
+				<?php } elseif ($location_valid && !$refer_req && $accept_new && $portal_section_show) { ?>
 					<p>New patients can make an appointment <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.
 					<?php } else { ?>
 						<?php echo $appointment_reference_direct; ?>.
 					<?php } ?></p>
 					<p>Existing patients also have the option of <?php echo $appointment_reference_portal; ?>.</p>
-				<?php } elseif (!$location_valid && !$refer_req && $accept_new && !$show_portal) {
+				<?php } elseif (!$location_valid && !$refer_req && $accept_new && !$portal_section_show) {
 					// Showing main appointment line reference here regardless of 
 					// decision on main appointment line, as there is no valid location 
 					// in this scenario. There would otherwise be no manner to make an 
 					// appointment with this provider.
 				?>
 					<p>New and existing patients can make an appointment <?php echo $appointment_reference_main; ?>.</p>
-				<?php } elseif ($location_valid && !$refer_req && $accept_new && !$show_portal) { ?>
+				<?php } elseif ($location_valid && !$refer_req && $accept_new && !$portal_section_show) { ?>
 					<p>New and existing patients can make an appointment <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.
 					<?php } else { ?>
 						<?php echo $appointment_reference_direct; ?>.
 					<?php } ?></p>
-				<?php } elseif ($location_valid && !$refer_req && !$accept_new && $show_portal) { ?>
+				<?php } elseif ($location_valid && !$refer_req && !$accept_new && $portal_section_show) { ?>
 					<p>This <?php echo strtolower($provider_single_name); ?> is not currently accepting new patients.</p>
 					<p>Existing patients can make an appointment by <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_portal; ?>, <?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.</p>
 					<?php } else { ?>
 						<?php echo $appointment_reference_portal; ?> or <?php echo $appointment_reference_direct; ?>.</p>
 					<?php } ?>
-				<?php } elseif (!$location_valid && !$refer_req && !$accept_new && !$show_portal) {
+				<?php } elseif (!$location_valid && !$refer_req && !$accept_new && !$portal_section_show) {
 					// Showing main appointment line reference here regardless of 
 					// decision on main appointment line, as there is no valid location 
 					// in this scenario. There would otherwise be no manner to make an 
@@ -121,7 +121,7 @@ $appointment_reference_main = 'by calling ' . $appointment_phone_name . ' at <a 
 				?>
 					<p>This <?php echo strtolower($provider_single_name); ?> is not currently accepting new patients.</p>
 					<p>Existing patients can make an appointment <?php echo $appointment_reference_main; ?>.</p>
-				<?php } else { // if ($location_valid && !$refer_req && !$accept_new && !$show_portal) ?>
+				<?php } else { // if ($location_valid && !$refer_req && !$accept_new && !$portal_section_show) ?>
 					<p>This <?php echo strtolower($provider_single_name); ?> is not currently accepting new patients.</p>
 					<p>Existing patients can make an appointment <?php if ( $appointment_show_main_line_sys ) { ?>
 						<?php echo $appointment_reference_direct; ?> or <?php echo $appointment_reference_main; ?>.
