69e78154063f79e31771f8806fdae4b99b1f8526
Josh <joshdaugherty@gmail.com>
2023-06-21 16:05:34 -0500

    [FD20-3261] Nest "more" var definitions under a var check

--- includes/class.fad-functions.php
+++ includes/class.fad-functions.php
@@ -4985,11 +4985,11 @@ function uamswp_fad_section_clinical_resource() {
 		global $clinical_resource_section_show_header; // Query whether to display the section header // bool (default: true)
 		global $clinical_resource_section_title; // Text to use for the section title // string (default: Find-a-Doc Settings value for clinical resources section title in a general placement)
 		global $clinical_resource_section_intro; // Text to use for the section intro text // string (default: Find-a-Doc Settings value for clinical resources section intro text in a general placement)
+		global $clinical_resource_section_more_show; // Query whether to show the section that links to more items // bool (default: true)
 		global $clinical_resource_section_more_text; // Text to use for the "more" intro text // string (default: Find-a-Doc Settings value for clinical resources section "more" intro text in a general placement)
 		global $clinical_resource_section_more_link_text; // Text to use for the "more" link text // string (default: Find-a-Doc Settings value for clinical resources section "more" link text in a general placement)
 		global $clinical_resource_section_more_link_descr; // Text to use for the "more" link description // string (default: Find-a-Doc Settings value for clinical resources section "more" link description in a general placement)
 		global $clinical_resource_section_collapse_list; // Query whether to collapse the list of locations in the locations section // bool (default: false)
-		global $clinical_resource_section_more_show; // Query whether to show the section that links to more items // bool (default: true)
 
 		// Defined in uamswp_fad_labels_clinical_resource()
 		global $clinical_resource_single_name; // string
@@ -5030,37 +5030,39 @@ function uamswp_fad_section_clinical_resource() {
 				}
 				$clinical_resource_section_intro = $clinical_resource_fpage_intro_general;
 			}
-			if ( !isset($clinical_resource_section_more_text) ) {
-				// Set the section title using the system settings for the section title in a general placement
-				if ( !isset($clinical_resource_fpage_more_text_general) ) {
-					uamswp_fad_clinical_resource_fpage_text_general();
-					global $clinical_resource_fpage_more_text_general;
-				}
-				$clinical_resource_section_more_text = $clinical_resource_fpage_more_text_general;
-			}
-			if ( !isset($clinical_resource_section_more_link_text) ) {
-				// Set the section title using the system settings for the section title in a general placement
-				if ( !isset($clinical_resource_fpage_more_link_text_general) ) {
-					uamswp_fad_clinical_resource_fpage_text_general();
-					global $clinical_resource_fpage_more_link_text_general;
-				}
-				$clinical_resource_section_more_link_text = $clinical_resource_fpage_more_link_text_general;
-			}
-			if ( !isset($clinical_resource_section_more_link_descr) ) {
-				// Set the section title using the system settings for the section title in a general placement
-				if ( !isset($clinical_resource_fpage_more_link_descr_general) ) {
-					uamswp_fad_clinical_resource_fpage_text_general();
-					global $clinical_resource_fpage_more_link_descr_general;
-				}
-				$clinical_resource_section_more_link_descr = $clinical_resource_fpage_more_link_descr_general;
-			}
-			$clinical_resource_section_collapse_list = isset($clinical_resource_section_collapse_list) ? $clinical_resource_section_collapse_list : false;
 			$clinical_resource_section_more_show = isset($clinical_resource_section_more_show) ? $clinical_resource_section_more_show : true;
+			if ( $clinical_resource_section_more_show ) {
+				if ( !isset($clinical_resource_section_more_text) ) {
+					// Set the section title using the system settings for the section title in a general placement
+					if ( !isset($clinical_resource_fpage_more_text_general) ) {
+						uamswp_fad_clinical_resource_fpage_text_general();
+						global $clinical_resource_fpage_more_text_general;
+					}
+					$clinical_resource_section_more_text = $clinical_resource_fpage_more_text_general;
+				}
+				if ( !isset($clinical_resource_section_more_link_text) ) {
+					// Set the section title using the system settings for the section title in a general placement
+					if ( !isset($clinical_resource_fpage_more_link_text_general) ) {
+						uamswp_fad_clinical_resource_fpage_text_general();
+						global $clinical_resource_fpage_more_link_text_general;
+					}
+					$clinical_resource_section_more_link_text = $clinical_resource_fpage_more_link_text_general;
+				}
+				if ( !isset($clinical_resource_section_more_link_descr) ) {
+					// Set the section title using the system settings for the section title in a general placement
+					if ( !isset($clinical_resource_fpage_more_link_descr_general) ) {
+						uamswp_fad_clinical_resource_fpage_text_general();
+						global $clinical_resource_fpage_more_link_descr_general;
+					}
+					$clinical_resource_section_more_link_descr = $clinical_resource_fpage_more_link_descr_general;
+				}
+				$clinical_resource_section_more_link_descr_attr = uamswp_attr_conversion($clinical_resource_section_more_link_descr);
+				$clinical_resource_section_more_link_url = '/clinical-resource/?' . $clinical_resource_section_more_link_key . '=' . $clinical_resource_section_more_link_value;
+				$clinical_resource_section_more_link_target = '_blank';	
+			}
 
-			$clinical_resource_section_more_link_descr_attr = uamswp_attr_conversion($clinical_resource_section_more_link_descr);
-			$clinical_resource_section_more_link_url = '/clinical-resource/?' . $clinical_resource_section_more_link_key . '=' . $clinical_resource_section_more_link_value;
-			$clinical_resource_section_more_link_target = '_blank';
-		
+			$clinical_resource_section_collapse_list = isset($clinical_resource_section_collapse_list) ? $clinical_resource_section_collapse_list : false;
+
 		?>
 		<section class="uams-module<?php echo $clinical_resource_section_class ? ' ' . $clinical_resource_section_class : ''; ?> bg-auto<?php echo $clinical_resource_section_collapse_list ? ' collapse-list' : ''; ?>"<?php echo $clinical_resource_section_id ? ' id="' . $clinical_resource_section_id . '" aria-labelledby="' . $clinical_resource_section_id . '-title"' : ''; ?>>
 			<div class="container-fluid">
