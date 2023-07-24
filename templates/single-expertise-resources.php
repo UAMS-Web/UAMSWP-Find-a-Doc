5ee8faf1eaa694300f495b16638530d26afdbcee
joshdaugherty <joshdaugherty@gmail.com>
2023-07-24 10:36:47 -0500

    [FD20-3331] Fix PHP errors
    Focused on:
    templates/single-expertise-resources.php

--- templates/single-expertise-resources.php
+++ templates/single-expertise-resources.php
@@ -249,7 +249,7 @@ $ontology_site_values_vars = isset($ontology_site_values_vars) ? $ontology_site_
 	// Query for whether related clinical resources content section should be displayed on ontology pages/subsections
 	$posts_per_page_clinical_resource_general_vars = isset($posts_per_page_clinical_resource_general_vars) ? $posts_per_page_clinical_resource_general_vars : uamswp_fad_posts_per_page_clinical_resource_general();
 		$clinical_resource_posts_per_page_fpage = $posts_per_page_clinical_resource_general_vars['clinical_resource_posts_per_page_fpage']; // int
-	$clinical_resource_posts_per_page = $clinical_resource_posts_per_page_section;
+	$clinical_resource_posts_per_page = $clinical_resource_posts_per_page_fpage;
 	$clinical_resource_query_vars = isset($clinical_resource_query_vars) ? $clinical_resource_query_vars : uamswp_fad_clinical_resource_query(
 		$clinical_resources,
 		$clinical_resource_posts_per_page
