b0a03e0c0182988facee4e516857fa06b2fde82f
Josh <joshdaugherty@gmail.com>
2023-07-26 20:53:40 -0500

    [FD20-3331] Remove empty optional arguments at end

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -778,8 +778,6 @@ while ( have_posts() ) : the_post(); ?>
 							$location_city, // string (optional) // The locality in which the street address is, and which is in the region. For example, Mountain View.
 							$location_state, // string (optional) // The region in which the locality is, and which is in the country. For example, California or another appropriate first-level Administrative division.
 							$location_zip, // string (optional) // The postal code. For example, 94043.
-							'', // string (optional) // The country. For example, USA. You can also provide the two-letter ISO 3166-1 alpha-2 country code.
-							'' // string (optional) // The telephone number.
 						);
 
 					// Geo Schema Data
