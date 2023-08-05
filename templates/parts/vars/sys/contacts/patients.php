276abcb4a74186a75e516a749dff47ca2ca0e831
Josh <joshdaugherty@gmail.com>
2023-08-05 14:29:51 -0500

    [FD20-3331] Add $appointment_* to parts

---
+++ templates/parts/vars/sys/contacts/patients.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * Template Name: System settings for general patient appointment information
+ * 
+ * Description: A template part that defines a series of variables related to the 
+ * phone numbers and URLs used for patients to make an appointment.
+ */
+
+if (
+	!isset($appointment_patients_phone_number_new) || empty($appointment_patients_phone_number_new)
+	||
+	!isset($appointment_patients_phone_label_new) || empty($appointment_patients_phone_label_new)
+	||
+	!isset($appointment_patients_phone_label_new_attr) || empty($appointment_patients_phone_label_new_attr)
+	||
+	!isset($appointment_patients_phone_info_new) || empty($appointment_patients_phone_info_new)
+	||
+	!isset($appointment_patients_phone_number_existing) || empty($appointment_patients_phone_number_existing)
+	||
+	!isset($appointment_patients_phone_label_existing) || empty($appointment_patients_phone_label_existing)
+	||
+	!isset($appointment_patients_phone_label_existing_attr) || empty($appointment_patients_phone_label_existing_attr)
+	||
+	!isset($appointment_patients_phone_info_existing) || empty($appointment_patients_phone_info_existing)
+	||
+	!isset($appointment_patients_phone_number_both) || empty($appointment_patients_phone_number_both)
+	||
+	!isset($appointment_patients_phone_label_both) || empty($appointment_patients_phone_label_both)
+	||
+	!isset($appointment_patients_phone_label_both_attr) || empty($appointment_patients_phone_label_both_attr)
+	||
+	!isset($appointment_patients_phone_info_both) || empty($appointment_patients_phone_info_both)
+	||
+	!isset($appointment_patients_web_url_new) || empty($appointment_patients_web_url_new)
+	||
+	!isset($appointment_patients_web_label_new) || empty($appointment_patients_web_label_new)
+	||
+	!isset($appointment_patients_web_label_new_attr) || empty($appointment_patients_web_label_new_attr)
+	||
+	!isset($appointment_patients_web_info_new) || empty($appointment_patients_web_info_new)
+	||
+	!isset($appointment_patients_web_url_existing) || empty($appointment_patients_web_url_existing)
+	||
+	!isset($appointment_patients_web_label_existing) || empty($appointment_patients_web_label_existing)
+	||
+	!isset($appointment_patients_web_label_existing_attr) || empty($appointment_patients_web_label_existing_attr)
+	||
+	!isset($appointment_patients_web_info_existing) || empty($appointment_patients_web_info_existing)
+	||
+	!isset($appointment_patients_web_url_both) || empty($appointment_patients_web_url_both)
+	||
+	!isset($appointment_patients_web_label_both) || empty($appointment_patients_web_label_both)
+	||
+	!isset($appointment_patients_web_label_both_attr) || empty($appointment_patients_web_label_both_attr)
+	||
+	!isset($appointment_patients_web_info_both) || empty($appointment_patients_web_info_both)
+) {
+
+	$appointment_patients_vars = uamswp_fad_appointment_patients();
+		$appointment_patients_phone_number_new = $appointment_patients_vars['appointment_patients_phone_number_new'];  // string
+		$appointment_patients_phone_label_new = $appointment_patients_vars['appointment_patients_phone_label_new'];  // string
+		$appointment_patients_phone_label_new_attr = $appointment_patients_vars['appointment_patients_phone_label_new_attr'];  // string
+		$appointment_patients_phone_info_new = $appointment_patients_vars['appointment_patients_phone_info_new'];  // string
+		$appointment_patients_phone_number_existing = $appointment_patients_vars['appointment_patients_phone_number_existing'];  // string
+		$appointment_patients_phone_label_existing = $appointment_patients_vars['appointment_patients_phone_label_existing'];  // string
+		$appointment_patients_phone_label_existing_attr = $appointment_patients_vars['appointment_patients_phone_label_existing_attr'];  // string
+		$appointment_patients_phone_info_existing = $appointment_patients_vars['appointment_patients_phone_info_existing'];  // string
+		$appointment_patients_phone_number_both = $appointment_patients_vars['appointment_patients_phone_number_both'];  // string
+		$appointment_patients_phone_label_both = $appointment_patients_vars['appointment_patients_phone_label_both'];  // string
+		$appointment_patients_phone_label_both_attr = $appointment_patients_vars['appointment_patients_phone_label_both_attr'];  // string
+		$appointment_patients_phone_info_both = $appointment_patients_vars['appointment_patients_phone_info_both'];  // string
+		$appointment_patients_web_url_new = $appointment_patients_vars['appointment_patients_web_url_new'];  // string
+		$appointment_patients_web_label_new = $appointment_patients_vars['appointment_patients_web_label_new'];  // string
+		$appointment_patients_web_label_new_attr = $appointment_patients_vars['appointment_patients_web_label_new_attr'];  // string
+		$appointment_patients_web_info_new = $appointment_patients_vars['appointment_patients_web_info_new'];  // string
+		$appointment_patients_web_url_existing = $appointment_patients_vars['appointment_patients_web_url_existing'];  // string
+		$appointment_patients_web_label_existing = $appointment_patients_vars['appointment_patients_web_label_existing'];  // string
+		$appointment_patients_web_label_existing_attr = $appointment_patients_vars['appointment_patients_web_label_existing_attr'];  // string
+		$appointment_patients_web_info_existing = $appointment_patients_vars['appointment_patients_web_info_existing'];  // string
+		$appointment_patients_web_url_both = $appointment_patients_vars['appointment_patients_web_url_both'];  // string
+		$appointment_patients_web_label_both = $appointment_patients_vars['appointment_patients_web_label_both'];  // string
+		$appointment_patients_web_label_both_attr = $appointment_patients_vars['appointment_patients_web_label_both_attr'];  // string
+		$appointment_patients_web_info_both = $appointment_patients_vars['appointment_patients_web_info_both'];  // string
+
+}
---
+++ templates/parts/vars/sys/contacts/refer.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Template Name: System settings for general patient referral information
+ * 
+ * Description: A template part that defines a series of variables related to the 
+ * phone numbers and URLs used for physicians to submit appointment referrals on 
+ * the behalf of patients.
+ */
+
+if (
+	!isset($appointment_refer_phone_number) || empty($appointment_refer_phone_number)
+	||
+	!isset($appointment_refer_phone_label) || empty($appointment_refer_phone_label)
+	||
+	!isset($appointment_refer_phone_label_attr) || empty($appointment_refer_phone_label_attr)
+	||
+	!isset($appointment_refer_phone_info) || empty($appointment_refer_phone_info)
+	||
+	!isset($appointment_refer_fax_number) || empty($appointment_refer_fax_number)
+	||
+	!isset($appointment_refer_fax_label) || empty($appointment_refer_fax_label)
+	||
+	!isset($appointment_refer_fax_label_attr) || empty($appointment_refer_fax_label_attr)
+	||
+	!isset($appointment_refer_fax_info) || empty($appointment_refer_fax_info)
+	||
+	!isset($appointment_refer_web_url) || empty($appointment_refer_web_url)
+	||
+	!isset($appointment_refer_web_label) || empty($appointment_refer_web_label)
+	||
+	!isset($appointment_refer_web_label_attr) || empty($appointment_refer_web_label_attr)
+	||
+	!isset($appointment_refer_web_info) || empty($appointment_refer_web_info)
+) {
+
+	$appointment_refer_vars = uamswp_fad_appointment_refer();
+		$appointment_refer_phone_number = $appointment_refer_vars['appointment_refer_phone_number'];  // string
+		$appointment_refer_phone_label = $appointment_refer_vars['appointment_refer_phone_label'];  // string
+		$appointment_refer_phone_label_attr = $appointment_refer_vars['appointment_refer_phone_label_attr'];  // string
+		$appointment_refer_phone_info = $appointment_refer_vars['appointment_refer_phone_info'];  // string
+		$appointment_refer_fax_number = $appointment_refer_vars['appointment_refer_fax_number'];  // string
+		$appointment_refer_fax_label = $appointment_refer_vars['appointment_refer_fax_label'];  // string
+		$appointment_refer_fax_label_attr = $appointment_refer_vars['appointment_refer_fax_label_attr'];  // string
+		$appointment_refer_fax_info = $appointment_refer_vars['appointment_refer_fax_info'];  // string
+		$appointment_refer_web_url = $appointment_refer_vars['appointment_refer_web_url'];  // string
+		$appointment_refer_web_label = $appointment_refer_vars['appointment_refer_web_label'];  // string
+		$appointment_refer_web_label_attr = $appointment_refer_vars['appointment_refer_web_label_attr'];  // string
+		$appointment_refer_web_info = $appointment_refer_vars['appointment_refer_web_info'];  // string
+
+}
