0ad06feb83031b40bd3d39f662089814de23d27f
Josh <joshdaugherty@gmail.com>
2023-08-08 14:18:27 -0500

    [FD20-3331] Apply clin.resource card vars function to card parts

--- templates/parts/html/cards/clinical-resource.php
+++ templates/parts/html/cards/clinical-resource.php
@@ -12,73 +12,77 @@
 
 // Check/define variables
 
+	// Page ID
 	$page_id = get_the_ID();
 
-	// Get system settings for clinical resource labels
-	include( UAMS_FAD_PATH . '/templates/parts/vars/sys/labels/clinical-resource.php' );
+	// Get the field values for the card
 
-$resource_title = get_the_title($page_id);
-$resource_title_attr = uamswp_attr_conversion($resource_title);
+		$clinical_resource_card_fields_vars = ''; // Reset the variables
+		$clinical_resource_card_style = 'basic'; // Clinical resource card style
+		include( UAMS_FAD_PATH . '/templates/parts/vars/page/cards/clinical-resource.php' );
 
-$resource_type = get_field('clinical_resource_type', $page_id);
-$resource_type_value = $resource_type['value'];
-$resource_type_label = $resource_type['label'];
-
-// Build an array of resource type values (keys) with the corresponding button text (values)
-$resource_button_text_arr = array(
-	'text' => 'Read the Article',
-	'infographic' => 'View the Infographic',
-	'video' => 'Watch the Video',
-	'doc' => 'Read the Document'
-);
-$resource_button_text = 'View the ' . $clinical_resource_single_name; // Set fallback button text
-if ( isset($resource_type) && isset($resource_button_text_arr[$resource_type_value]) ) {
-	// IF resource type is set...
-	// AND IF the resource type value is in $resource_button_text_arr...
-	$resource_button_text = $resource_button_text_arr[$resource_type_value]; // Set the button text from the corresponding value from the array
-}
-$resource_button_text_attr = uamswp_attr_conversion($resource_button_text);
-$resource_label = $resource_button_text_attr . ', ' . $resource_title_attr;
-
-$resource_excerpt = get_the_excerpt($page_id) ? get_the_excerpt($page_id) : wp_strip_all_tags( get_the_content($page_id) );
-$resource_excerpt_len = strlen($resource_excerpt);
-if ( $resource_excerpt_len > 160 ) {
-	$resource_excerpt = wp_trim_words( $resource_excerpt, 23, ' &hellip;' );
-}
-
-$resource_image_wide = get_post_thumbnail_id($page_id);
+// Construct the card
 
 ?>
 <div class="item">
 	<div class="card">
 		<div class="card-img-top">
 			<picture>
-				<?php if ( has_post_thumbnail($page_id) && function_exists( 'fly_add_image_size' ) ) { ?> 
-					<source srcset="<?php echo image_sizer($resource_image_wide, 455, 256, 'center', 'center'); ?>" 
-						media="(min-width: 1921px)">
-					<source srcset="<?php echo image_sizer($resource_image_wide, 433, 244, 'center', 'center'); ?>" 
-						media="(min-width: 1500px)">
-					<source srcset="<?php echo image_sizer($resource_image_wide, 455, 256, 'center', 'center'); ?>" 
-						media="(min-width: 992px)">
-					<source srcset="<?php echo image_sizer($resource_image_wide, 433, 244, 'center', 'center'); ?>" 
-						media="(min-width: 768px)">
-					<source srcset="<?php echo image_sizer($resource_image_wide, 455, 256, 'center', 'center'); ?>" 
-						media="(min-width: 1px)">
-					<!-- Fallback -->
-					<img src="<?php echo image_sizer($resource_image_wide, 455, 256, 'center', 'center'); ?>" alt="" role="presentation" />
-				<?php } elseif ( has_post_thumbnail($page_id) ) { ?>
-					<!-- Fallback -->
-					<?php the_post_thumbnail( 'aspect-16-9-small', array( 'alt' => '', 'role' => 'presentation' ) ); ?>
-				<?php } else { ?>
+				<?php
+
+				if ( $clinical_resource_featured_image_base_url ) {
+
+					foreach ( $clinical_resource_featured_image_srcset as $srcset ) {
+
+						?>
+						<source srcset="<?php echo $srcset['url']; ?>" media="(min-width: <?php echo $srcset['media-min-width']; ?>)">
+						<?php
+
+					}
+
+					?>
+					<img src="<?php echo $clinical_resource_featured_image_base_url; ?>" alt="" role="presentation" loading="lazy" />
+					<?php
+
+				} else {
+
+					?>
 					<source srcset="/wp-content/plugins/UAMSWP-Find-a-Doc/assets/svg/no-image_16-9.svg" media="(min-width: 1px)">
-					<img src="/wp-content/plugins/UAMSWP-Find-a-Doc/assets/svg/no-image_16-9.jpg" alt="" role="presentation" />
-				<?php } ?>
+					<img src="/wp-content/plugins/UAMSWP-Find-a-Doc/assets/svg/no-image_16-9.jpg" alt="" role="presentation" loading="lazy" />
+					<?php
+
+				} // endif ( $clinical_resource_featured_image_base_url ) else
+
+				?>
 			</picture>
 		</div>
 		<div class="card-body">
-			<h3 class="card-title h5"><?php echo $resource_title; ?> <span class="subtitle"><span class="sr-only">(</span><?php echo esc_html($resource_type_label); ?><span class="sr-only">)</span></span></h3>
-			<p class="card-text"><?php echo $resource_excerpt; ?></p>
-			<a href="<?php echo get_permalink($page_id); ?>" class="btn btn-primary stretched-link" aria-label="<?php echo $resource_label; ?>" data-itemtitle="<?php echo $resource_title_attr; ?>"><?php echo $resource_button_text; ?></a>
+			<h3 class="card-title h5">
+				<span class="name"><?php echo $clinical_resource_title; ?></span>
+				<?php
+
+				if ( $clinical_resource_type_label ) {
+
+					?>
+					<span class="subtitle"><span class="sr-only">(</span><?php echo esc_html($clinical_resource_type_label); ?><span class="sr-only">)</span></span>
+					<?php
+
+				} // endif ( $clinical_resource_type_label )
+
+				?>
+			</h3>
+			<?php
+
+			if ( $clinical_resource_excerpt ) {
+
+				?>
+				<p class="card-text"><?php echo $clinical_resource_excerpt; ?></p>
+				<?php
+
+			} // endif ( $clinical_resource_excerpt )
+
+			?>
+			<a href="<?php echo $clinical_resource_url; ?>" class="btn btn-primary stretched-link" aria-label="<?php echo $clinical_resource_link_label; ?>" data-itemtitle="<?php echo $clinical_resource_title_attr; ?>"><?php echo $clinical_resource_link_text; ?></a>
 		</div>
 	</div>
 </div>
