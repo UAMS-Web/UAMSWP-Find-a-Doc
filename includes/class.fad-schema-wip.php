f1f932b834fafdec098415a2ef85ee31a54bdbf6
Josh <joshdaugherty@gmail.com>
2023-08-13 11:43:07 -0500

    [FD20-3331] Add framework for AnatomicalStructure

--- includes/class.fad-schema-wip.php
+++ includes/class.fad-schema-wip.php
@@ -37561,6 +37561,124 @@
 		}
 
 		// AnatomicalStructure
+
+			/*
+			 * 
+			 */
+
+			function uamswp_fad_schema_anatomicalstructure(
+				$schema, // array // Main schema array
+				// AnatomicalStructure
+					$foo = '', // foo
+				// MedicalEntity
+					$code = '', // code
+					$funding = '', // funding
+					$guideline = '', // guideline
+					$legalStatus = '', // legalStatus
+					$medicineSystem = '', // medicineSystem
+					$recognizingAuthority = '', // recognizingAuthority
+					$relevantSpecialty = '', // relevantSpecialty
+					$study = '', // study
+				// Thing
+					$additionalType = '', // additionalType
+					$alternateName = '', // alternateName
+					$description = '', // description
+					$disambiguatingDescription = '', // disambiguatingDescription
+					$identifier = '', // identifier
+					$image = '', // image
+					$mainEntityOfPage = '', // mainEntityOfPage
+					$name = '', // name
+					$potentialAction = '', // potentialAction
+					$sameAs = '', // sameAs
+					$subjectOf = '', // subjectOf
+					$url = '' // url
+			) {
+	
+				// Check/define variables
+	
+					$schema = ( isset($schema) && is_array($schema) && !empty($schema) ) ? $schema : array();
+	
+					// Inherited properties from Thing
+	
+						$additionalType = ( isset($additionalType) && !empty($additionalType) ) ? $additionalType : '';
+						$alternateName = ( isset($alternateName) && !empty($alternateName) ) ? $alternateName : '';
+						$description = ( isset($description) && !empty($description) ) ? $description : '';
+						$disambiguatingDescription = ( isset($disambiguatingDescription) && !empty($disambiguatingDescription) ) ? $disambiguatingDescription : '';
+						$identifier = ( isset($identifier) && !empty($identifier) ) ? $identifier : '';
+						$image = ( isset($image) && !empty($image) ) ? $image : '';
+						$mainEntityOfPage = ( isset($mainEntityOfPage) && !empty($mainEntityOfPage) ) ? $mainEntityOfPage : '';
+						$name = ( isset($name) && !empty($name) ) ? $name : '';
+						$potentialAction = ( isset($potentialAction) && !empty($potentialAction) ) ? $potentialAction : '';
+						$sameAs = ( isset($sameAs) && !empty($sameAs) ) ? $sameAs : '';
+						$subjectOf = ( isset($subjectOf) && !empty($subjectOf) ) ? $subjectOf : '';
+						$url = ( isset($url) && !empty($url) ) ? $url : '';
+	
+					// Inherited properties from MedicalEntity (Thing > MedicalEntity)
+	
+						$code = ( isset($code) && !empty($code) ) ? $code : '';
+						$funding = ( isset($funding) && !empty($funding) ) ? $funding : '';
+						$guideline = ( isset($guideline) && !empty($guideline) ) ? $guideline : '';
+						$legalStatus = ( isset($legalStatus) && !empty($legalStatus) ) ? $legalStatus : '';
+						$medicineSystem = ( isset($medicineSystem) && !empty($medicineSystem) ) ? $medicineSystem : '';
+						$recognizingAuthority = ( isset($recognizingAuthority) && !empty($recognizingAuthority) ) ? $recognizingAuthority : '';
+						$relevantSpecialty = ( isset($relevantSpecialty) && !empty($relevantSpecialty) ) ? $relevantSpecialty : '';
+						$study = ( isset($study) && !empty($study) ) ? $study : '';
+				
+					// Properties from AnatomicalStructure (Thing > MedicalEntity > AnatomicalStructure)
+
+						$foo = ( isset($foo) && !empty($foo) ) ? $foo : '';
+
+				// Add values to the schema array
+	
+					// Inherited properties
+	
+						$schema = uamswp_fad_schema_medicalentity(
+							$schema, // array // Main schema array
+							// MedicalEntity
+								$code, // code
+								$funding, // funding
+								$guideline, // guideline
+								$legalStatus, // legalStatus
+								$medicineSystem, // medicineSystem
+								$recognizingAuthority, // recognizingAuthority
+								$relevantSpecialty, // relevantSpecialty
+								$study, // study
+							// Thing
+								$additionalType, // additionalType
+								$alternateName, // alternateName
+								$description, // description
+								$disambiguatingDescription, // disambiguatingDescription
+								$identifier, // identifier
+								$image, // image
+								$mainEntityOfPage, // mainEntityOfPage
+								$name, // name
+								$potentialAction, // potentialAction
+								$sameAs, // sameAs
+								$subjectOf, // subjectOf
+								$url // url
+						);
+	
+					// Properties from MedicalEntity
+	
+						// foo
+	
+							/* 
+							 * Expected Type:
+							 *     Bar
+							 * 
+							 * 
+							 */
+	
+							$schema['foo'] = $foo;
+	
+				// Remove any empty values from the schema array
+	
+					$schema = array_filter($schema);
+	
+				return $schema;
+	
+			}
+
 		// AnatomicalSystem
 		// DrugClass
 		// DrugCost
