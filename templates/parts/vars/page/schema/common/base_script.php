8d97283ad7e5bc0becbee25ceb4113a25c08802d
joshdaugherty <joshdaugherty@gmail.com>
2023-10-24 08:30:32 -0500

    [FD20-3311] Remove website schema item from base script

--- templates/parts/vars/page/schema/common/base_script.php
+++ templates/parts/vars/page/schema/common/base_script.php
@@ -5,10 +5,3 @@ $schema_common_base = array(
 	'@graph' => array(
 	)
 );
-
-// Add UAMS items to the array
-
-	include( UAMS_FAD_PATH . '/templates/parts/vars/page/schema/common/uams.php' );
-
-	// UAMSHealth.com as WebSite
-	$schema_common_base['@graph'][] = $schema_base_website_uams_health;
