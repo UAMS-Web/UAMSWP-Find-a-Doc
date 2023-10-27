f2e2164fea4fa99f6ba73b29eb263ca1cb9f175f
Josh <joshdaugherty@gmail.com>
2023-10-27 10:00:45 -0500

    [FD20-3311] Code refactoring
    tabs and trailing spaces

--- admin/js/acf-admin.js
+++ admin/js/acf-admin.js
@@ -1,31 +1,31 @@
 var hm_googlemap = null;
 
 if (eval("typeof acf") != undefined) {
-  console.log('ACF IS HERE!');  
-  
-  acf.addAction('google_map_init', function(map, marker, field) {
-    hm_googlemap = map;
+	console.log('ACF IS HERE!');
 
-    //Override the searchPosition function on the 
-    //map ACF Field which normally does a geocode search
-    field.__proto__.searchPosition = function(lat, lng) {	      
-      var zoom = (hm_googlemap !== null) ? hm_googlemap.zoom : 15;
-      var val = {
-        address: "Custom Location: (" + lat + ", " + lng + ")",
-        lat: lat,
-        lng: lng,
-        zoom: zoom,
-        country: "", 
-        country_short: "", 
-        place_id: "",
-        post_code: "", 
-        state: "", 
-        street_name: "", 
-        street_name_short: "",
-      };
-      
-//console.log(val);
-	  this.val(val);				
-    };    
-  });  
+	acf.addAction('google_map_init', function(map, marker, field) {
+		hm_googlemap = map;
+
+		//Override the searchPosition function on the
+		//map ACF Field which normally does a geocode search
+		field.__proto__.searchPosition = function(lat, lng) {
+			var zoom = (hm_googlemap !== null) ? hm_googlemap.zoom : 15;
+			var val = {
+				address: "Custom Location: (" + lat + ", " + lng + ")",
+				lat: lat,
+				lng: lng,
+				zoom: zoom,
+				country: "",
+				country_short: "",
+				place_id: "",
+				post_code: "",
+				state: "",
+				street_name: "",
+				street_name_short: "",
+			};
+
+		//console.log(val);
+		this.val(val);
+		};
+	});
 }
