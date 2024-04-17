37cfa5a95fa461695987facd17c6dcbcc2e75745
joshdaugherty <joshdaugherty@gmail.com>
2024-04-17 10:53:59 -0500

    [FD20-3311] Move degree id array cleanup within if statement
    This cleanup is only necessary if the $provider_degrees_id variable had not already been defined.

--- includes/class.fad-schema.php
+++ includes/class.fad-schema.php
@@ -17205,17 +17205,17 @@
 
 														$provider_degrees_id = get_field( 'physician_degree', $entity );
 
+														// Clean up values
+
+															if ( $provider_degrees_id ) {
+
+																$provider_degrees_id = array_filter($provider_degrees_id);
+																$provider_degrees_id = array_unique( $provider_degrees_id, SORT_REGULAR );
+																$provider_degrees_id = array_values($provider_degrees_id);
+
+															}
+
 													}
-
-													// Clean up values
-
-														if ( $provider_degrees_id ) {
-
-															$provider_degrees_id = array_filter($provider_degrees_id);
-															$provider_degrees_id = array_unique( $provider_degrees_id, SORT_REGULAR );
-															$provider_degrees_id = array_values($provider_degrees_id);
-
-														}
 
 												// Format schema values
 
