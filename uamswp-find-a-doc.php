7e3ab46789c9b227c07da0d8115a226214788894
todd-uams <todd@uams.edu>
2026-05-20 10:34:45 -0500

    Update minor version

--- uamswp-find-a-doc.php
+++ uamswp-find-a-doc.php
@@ -5,7 +5,7 @@ Plugin URI: -
 Description: Find-a-doc plugin for uamshealth.com
 Author: uams, Todd McKee, MEd
 Author URI: https://www.uams.edu/
-Version: 2.2.12
+Version: 2.2.13
 */
 
 // If this file is called directly, abort.
@@ -14,7 +14,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // This plugin uses namespaces and requires PHP 5.3 or greater.
-if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+if ( version_compare( PHP_VERSION, '7.4', '<' ) ) {
 	add_action( 'admin_notices', create_function( '', // phpcs:ignore WordPress.PHP.RestrictedPHPFunctions.create_function_create_function
 	"echo '<div class=\"error\"><p>" . __( 'UAMSWP Find-a-doc requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'uamswp-find-a-doc' ) . "</p></div>';" ) );
 	return;
